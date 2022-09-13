4d75b553c7150012a7a4e1a824790273d9749ac9
Stéphane Blondon <stephane.blondon@gmail.com>
2022-09-13 17:59:58 +0200

    ajout d'une page de résultat du siren envoyé par l'utilisateur

--- impact/public/views.py
+++ impact/public/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "public/index.html")
+
+
+def siren(request):
+    siren = request.GET["siren"]
+    return render(request, "public/siren.html", {"siren": siren})
