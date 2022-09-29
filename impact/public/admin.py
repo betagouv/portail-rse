a3dfa81e87381f09a9556728166bda95baf0a552
emillumine <camille@yaal.coop>
2022-09-29 14:56:08 +0200

    ajoute Entreprise et BDESE dans l'admin

--- impact/public/admin.py
+++ impact/public/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import BDESE, Entreprise
+
+
+class CategoryJSONField(forms.JSONField):
+    def __init__(self, base_field, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class BDESEForm(forms.ModelForm):
+    class Meta:
+        model = BDESE
+        fields = "__all__"
+        field_classes = {"effectif_total": CategoryJSONField}
+
+
+@admin.register(BDESE)
+class BDESEAdmin(admin.ModelAdmin):
+    form = BDESEForm
+
+
+admin.site.register(Entreprise)
