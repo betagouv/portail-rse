39c04d747c2f1f3b1c0a932abd599a47bed58513
emillumine <camille@yaal.coop>
2022-09-29 15:56:00 +0200

    configure la base de données

--- impact/impact/settings.py
+++ impact/impact/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,12 +80,7 @@ WSGI_APPLICATION = "impact.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Password validation
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 python impact/manage.py collectstatic --no-input
+python impact/manage.py migrate
 gunicorn --pythonpath=impact impact.wsgi --log-file -
