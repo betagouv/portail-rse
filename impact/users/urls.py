e1a6a44db7b76866c4c5dcda24f246d363ddcae0
emillumine <camille@yaal.coop>
2022-10-14 11:09:39 +0200

    affiche des messages de succès dans le process de réinitialisation du mot de passe

--- impact/users/urls.py
+++ impact/users/urls.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from .forms import LoginForm, PasswordResetForm, SetPasswordForm
-from . import views
+from .views import creation, PasswordResetView, PasswordResetConfirmView
 
 urlpatterns = [
     path(
@@ -13,35 +13,25 @@ urlpatterns = [
         name="login",
     ),
     path("deconnexion", auth_views.LogoutView.as_view(), name="logout"),
-    path("creation", views.creation, name="creation"),
+    path("creation", creation, name="creation"),
     path(
-        "mot-de-passe",
-        auth_views.PasswordResetView.as_view(
+        "mot-de-passe-oublie",
+        PasswordResetView.as_view(
             template_name="users/password_reset_form.html",
             form_class=PasswordResetForm,
             email_template_name="users/email/password_reset_email.html",
             subject_template_name="users/email/password_reset_subject.txt",
             success_url="/",
-            # on pourrait faire une page de confirmation d'email envoyé à la place de success_url
         ),
         name="password_reset",
     ),
     path(
-        "mot-de-passe/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(
+        "mot-de-passe-oublie/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
             template_name="users/password_reset_confirm.html",
             form_class=SetPasswordForm,
-            # success_url="login",
-            # ne fonctionne pas alors que ça devrait ? du coup on utilise password_reset_complete ci-dessous
+            success_url=reverse_lazy("login"),
         ),
         name="password_reset_confirm",
     ),
-    path(
-        "mot-de-passe/reinitialise",
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name="users/password_reset_complete.html"
-        ),
-        name="password_reset_complete",
-    )
-    # non nécessaire si success_url fonctionne
 ]
--- impact/users/views.py
+++ impact/users/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib.auth.views import PasswordResetView as BasePasswordResetView
+from django.contrib.auth.views import PasswordResetConfirmView as BasePasswordResetConfirmView
 from django.shortcuts import redirect, render
 
 from .forms import UserCreationForm
@@ -16,3 +19,19 @@ def creation(request):
         siren = request.GET.get("siren")
         form = UserCreationForm(initial={"siren": siren})
     return render(request, "users/creation.html", {"form": form})
+
+
+class PasswordResetView(BasePasswordResetView):
+    def form_valid(self, form):
+        response = super(PasswordResetView, self).form_valid(form)
+        success_message = "Un lien de ré-initialisation de votre mot de passe vous a été envoyé par mail. Vous devriez le recevoir d'ici quelques minutes."
+        messages.success(self.request, success_message)
+        return response
+
+
+class PasswordResetConfirmView(BasePasswordResetConfirmView):
+    def form_valid(self, form):
+        response = super(PasswordResetConfirmView, self).form_valid(form)
+        success_message = "Votre mot de passe a été réinitialisé avec succès. Vous pouvez à présent vous connecter."
+        messages.success(self.request, success_message)
+        return response
