efee80450af35329b69bd3ea8bbdbe4e85dd7da4
emillumine <camille@yaal.coop>
2022-10-19 15:51:57 +0200

    refacto: factorise du code commun dans des dataclasses pour le rendu des différentes réglementations

--- impact/public/views.py
+++ impact/public/views.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass, field
+
 import requests
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import HttpResponse, render
 from django.template.loader import render_to_string
+from django.urls import reverse_lazy
 from weasyprint import HTML
 
 from .forms import bdese_form_factory, EligibiliteForm, SirenForm
@@ -65,19 +68,104 @@ def siren(request):
     return render(request, "public/index.html", {"form": form, "errors": errors})
 
 
-BDESE_ELIGIBILITE = {
-    "NON_ELIGIBLE": 0,
-    "ELIGIBLE_AVEC_ACCORD": 1,
-    "ELIGIBLE_INFERIEUR_300": 2,
-    "ELIGIBLE_INFERIEUR_500": 3,
-    "ELIGIBLE_SUPERIEUR_500": 4,
-}
+@dataclass
+class ReglementationAction:
+    url: str
+    title: str
 
-INDEX_EGAPRO_ELIGIBILITE = {
-        "NON_ELIGIBLE": 0,
-        "ELIGIBLE_ACTUALISE": 1,
-        "ELIGIBLE_A_ACTUALISER": 2,
-}
+
+@dataclass
+class Reglementation:
+    STATUS_ACTUALISE = "actualisé"
+    STATUS_A_ACTUALISER = "à actualiser"
+    STATUS_EN_COURS = "en cours"
+    STATUS_NON_SOUMIS = "non soumis"
+
+    status: str
+    status_detail: str
+
+
+@dataclass
+class _Entreprise:
+    effectif: str
+    accord: bool
+    raison_sociale: str
+    siren: str
+
+
+@dataclass
+class BDESEReglementation(Reglementation):
+    TYPE_AVEC_ACCORD = 1
+    TYPE_INFERIEUR_300 = 2
+    TYPE_INFERIEUR_500 = 3
+    TYPE_SUPERIEUR_500 = 4
+
+    primary_action: ReglementationAction | None = None
+    secondary_actions: list[ReglementationAction] = field(default_factory=list)
+    bdese_type: int | None = None
+
+    @classmethod
+    def calculate(cls, entreprise: _Entreprise) -> "BDESEReglementation":
+        if entreprise.effectif == "petit":
+            status = cls.STATUS_NON_SOUMIS
+            status_detail = "Vous n'êtes pas soumis à cette réglementation"
+            return cls(status, status_detail)
+        elif entreprise.accord:
+            status = cls.STATUS_A_ACTUALISER
+            status_detail = "Vous êtes soumis à cette réglementation. Vous avez un accord d'entreprise spécifique. Veuillez vous y référer."
+            primary_action = ReglementationAction(
+                "#", "Marquer ma BDESE comme actualisée"
+            )
+            bdese_type = cls.TYPE_AVEC_ACCORD
+            return cls(
+                status,
+                status_detail,
+                primary_action=primary_action,
+                bdese_type=bdese_type,
+            )
+        else:
+            if entreprise.effectif == "moyen":
+                bdese_type = cls.TYPE_INFERIEUR_300
+            elif entreprise.effectif == "grand":
+                bdese_type = cls.TYPE_INFERIEUR_500
+            else:
+                bdese_type = cls.TYPE_SUPERIEUR_500
+            status = cls.STATUS_A_ACTUALISER
+            status_detail = "Vous êtes soumis à cette réglementation. Nous allons vous aider à la remplir."
+            primary_action = ReglementationAction(
+                reverse_lazy("bdese", args=[entreprise.siren]), "Actualiser ma BDESE"
+            )
+            secondary_actions = [
+                ReglementationAction(
+                    reverse_lazy("result")
+                    + f"?raison_sociale={ entreprise.raison_sociale }&bdese={ bdese_type }",
+                    "Télécharger le pdf",
+                ),
+            ]
+            return cls(
+                status,
+                status_detail,
+                primary_action=primary_action,
+                secondary_actions=secondary_actions,
+                bdese_type=bdese_type,
+            )
+
+
+@dataclass
+class IndexEgaproReglementation(Reglementation):
+    @classmethod
+    def calculate(cls, entreprise: _Entreprise) -> "IndexEgaproReglementations":
+        if entreprise.effectif == "petit":
+            status = cls.STATUS_NON_SOUMIS
+            status_detail = "Vous n'êtes pas soumis à cette réglementation"
+        elif is_index_egapro_updated(entreprise.siren):
+            status = cls.STATUS_ACTUALISE
+            status_detail = "Vous êtes soumis à cette réglementation. Vous avez rempli vos obligations d'après les données disponibles sur Index Egapro."
+        else:
+            status = cls.STATUS_A_ACTUALISER
+            status_detail = "Vous êtes soumis à cette réglementation. Vous n'avez pas encore déclaré votre index sur Index Egapro."
+        return cls(status, status_detail)
+
 
 def is_index_egapro_updated(siren):
     url = f"https://index-egapro.travail.gouv.fr/api/search?q={siren}"
@@ -88,37 +176,18 @@ def is_index_egapro_updated(siren):
                 return True
     return False
 
+
 def eligibilite(request):
     form = EligibiliteForm(request.GET)
     if form.is_valid():
-        accord = form.cleaned_data["accord"]
-        effectif = form.cleaned_data["effectif"]
-        raison_sociale = form.cleaned_data["raison_sociale"]
-        siren = form.cleaned_data["siren"]
-
-        if effectif == "petit":
-            bdese_result = BDESE_ELIGIBILITE["NON_ELIGIBLE"]
-            index_egapro_result = INDEX_EGAPRO_ELIGIBILITE["NON_ELIGIBLE"]
-        else:
-            index_egapro_result = INDEX_EGAPRO_ELIGIBILITE["ELIGIBLE_ACTUALISE"] if is_index_egapro_updated(siren) else INDEX_EGAPRO_ELIGIBILITE["ELIGIBLE_A_ACTUALISER"]
-            if accord:
-                bdese_result = BDESE_ELIGIBILITE["ELIGIBLE_AVEC_ACCORD"]
-            elif effectif == "moyen":
-                bdese_result = BDESE_ELIGIBILITE["ELIGIBLE_INFERIEUR_300"]
-            elif effectif == "grand":
-                bdese_result = BDESE_ELIGIBILITE["ELIGIBLE_INFERIEUR_500"]
-            else:
-                bdese_result = BDESE_ELIGIBILITE["ELIGIBLE_SUPERIEUR_500"]
+        entreprise = _Entreprise(**form.cleaned_data)
     return render(
         request,
         "public/reglementations_result.html",
         {
-            "BDESE_ELIGIBILITE": BDESE_ELIGIBILITE,
-            "bdese_result": bdese_result,
-            "INDEX_EGAPRO_ELIGIBILITE": INDEX_EGAPRO_ELIGIBILITE,
-            "index_egapro_result": index_egapro_result,
-            "raison_sociale": raison_sociale,
-            "siren": siren,
+            "entreprise": entreprise,
+            "bdese": BDESEReglementation.calculate(entreprise),
+            "index_egapro": IndexEgaproReglementation.calculate(entreprise),
         },
     )
 
@@ -128,10 +197,18 @@ def reglementations(request):
 
 
 def result(request):
+    bdese_type = int(request.GET["bdese"])
+    if bdese_type == BDESEReglementation.TYPE_AVEC_ACCORD:
+        bdese = "REGLEMENTATION AVEC ACCORD"
+    elif bdese_type == BDESEReglementation.TYPE_INFERIEUR_300:
+        bdese = "REGLEMENTATION MOINS DE 300 SALARIES"
+    elif bdese_type == BDESEReglementation.TYPE_INFERIEUR_500:
+        bdese = "REGLEMENTATION PLUS DE 300 SALARIES"
+    elif bdese_type == BDESEReglementation.TYPE_SUPERIEUR_500:
+        bdese = "REGLEMENTATION PLUS DE 500 SALARIES"
     context = {
         "raison_sociale": request.GET["raison_sociale"],
-        "bdese": int(request.GET["bdese"]),
-        "BDESE_ELIGIBILITE": BDESE_ELIGIBILITE,
+        "bdese": bdese,
     }
     pdf_html = render_to_string("public/result_pdf.html", context)
     pdf_file = HTML(string=pdf_html).write_pdf()
@@ -181,5 +258,7 @@ def bdese(request, siren):
             print(form.errors)
     else:
         fetched_data = get_bdese_data_from_index_egapro(siren, 2021)
-        form = bdese_form_factory(categories_professionnelles, fetched_data=fetched_data, instance=bdese)
+        form = bdese_form_factory(
+            categories_professionnelles, fetched_data=fetched_data, instance=bdese
+        )
     return render(request, "public/bdese.html", {"form": form, "siren": siren})
