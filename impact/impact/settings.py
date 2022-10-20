bfcefd90865eeabc64fc61fc48aa9400818a0913
Stéphane Blondon <stephane.blondon@gmail.com>
2022-10-20 18:16:46 +0200

    page montrant les entreprises associées à un utilisateur

--- impact/impact/settings.py
+++ impact/impact/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "0.0.0.0,127.0.0.1").split(",")
 # Application definition
 INSTALLED_APPS = [
     "public",
+    "entreprises",
     "users",
     "django.contrib.admin",
     "django.contrib.auth",
