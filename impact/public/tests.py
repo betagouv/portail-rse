be4ae533647c04a310a234d2b805099b55223745
Stéphane Blondon <stephane.blondon@gmail.com>
2022-10-20 11:52:59 +0200

    refacto: suppression de code commenté

--- impact/public/tests.py
+++ impact/public/tests.py
@@ -1,13 +1,6 @@
 from django.test import Client
 
 
-# class IndexTest(TestCase):
-#     def test(self):
-#         response = self.client.get("/")
-
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, "<!-- page index -->")
-
 def test_index():
     response = Client().get("/")
 
