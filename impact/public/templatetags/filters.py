7520454ac84c145f448e6d21cfe28e4f6881bff7
Stéphane Blondon <stephane.blondon@gmail.com>
2022-10-20 19:31:05 +0200

    affichage oui/non de l'accord d'entreprise

--- impact/public/templatetags/filters.py
+++ impact/public/templatetags/filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def index(indexable, i):
     return indexable[i]
+
+
+@register.filter
+def translate_boolean(boolean):
+    return "oui" if boolean else "non"
