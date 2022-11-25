bd1b1814a505dc036845b42449c664aea331908d
Stéphane Blondon <stephane.blondon@gmail.com>
2022-11-25 15:49:27 +0100

    wording

--- impact/entreprises/models.py
+++ impact/entreprises/models.py
@@ -15,7 +15,7 @@ class Entreprise(models.Model):
     effectif = models.CharField(
         max_length=9,
         choices=EFFECTIF_CHOICES,
-        help_text="Saisissez le nombre de salariés",
+        help_text="Affinez le nombre de salariés détectés",
     )
     bdese_accord = models.BooleanField(default=False)
     raison_sociale = models.CharField(max_length=50, default="")
