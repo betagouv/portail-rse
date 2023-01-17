c3b5b81b5f9668b20cb9f43d4543353594d595f9
emillumine <camille@yaal.coop>
2023-01-17 15:55:00 +0100

    créé un composant svelte IndicateurExterne

---
+++ front/src/lib/IndicateurExterne.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let toggleId = undefined
+    export let fieldId = undefined
+
+    let checked = false
+
+    const handleToggle = () => {
+        if (checked) {
+            document.getElementById(fieldId).disabled = true
+        } else {
+            document.getElementById(fieldId).disabled = false
+        }
+
+    }
+</script>
+
+<div class="fr-toggle">
+    <input type="checkbox" class="fr-toggle__input" aria-describedby="{toggleId}-hint-text" id="{toggleId}" bind:checked on:change={handleToggle}>
+    <label class="fr-toggle__label" for="{toggleId}" data-fr-checked-label="Activé" data-fr-unchecked-label="Désactivé">Indicateur externe</label> 
+</div>
--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,18 @@
 import Counter from './lib/Counter.svelte'
+import IndicateurExterne from './lib/IndicateurExterne.svelte'
 
 const counter = new Counter({
   target: document.getElementById('counter'),
 })
+
+for (var indicateurExterne of document.getElementsByClassName("svelte-indicateur-externe")) {
+  new IndicateurExterne({
+    target: indicateurExterne,
+    props: {
+      toggleId: indicateurExterne.dataset.toggleId,
+      fieldId: indicateurExterne.dataset.fieldId
+    }
+  })
+}
 
 export default counter
