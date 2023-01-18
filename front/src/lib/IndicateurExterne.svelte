4648845924d614ec5319229ff3366138e3ff19dc
emillumine <camille@yaal.coop>
2023-01-18 14:20:05 +0100

    active et désactive les indicateurs externes

--- front/src/lib/IndicateurExterne.svelte
+++ front/src/lib/IndicateurExterne.svelte
@@ -2,19 +2,30 @@
     export let toggleId = undefined
     export let fieldId = undefined
 
-    let checked = false
+    let field = document.getElementById(fieldId)
+    let indicateursExternesField = document.getElementById("id_indicateurs_externes_in_step")
+
+    let checked = JSON.parse(indicateursExternesField.value).includes(field.name)
+    if (checked) {
+        field.disabled = true
+    }
 
     const handleToggle = () => {
+        const oldIndicateursExternes = JSON.parse(indicateursExternesField.value)
         if (checked) {
-            document.getElementById(fieldId).disabled = true
-        } else {
-            document.getElementById(fieldId).disabled = false
-        }
+            field.disabled = true
+            indicateursExternesField.value = JSON.stringify([...oldIndicateursExternes, field.name])
 
+        } else {
+            field.disabled = false
+            const newIndicateursExternes = oldIndicateursExternes.filter(fieldName => fieldName != field.name)
+            indicateursExternesField.value = JSON.stringify(newIndicateursExternes)
+
+        }
     }
 </script>
 
 <div class="fr-toggle">
-    <input type="checkbox" class="fr-toggle__input" aria-describedby="{toggleId}-hint-text" id="{toggleId}" bind:checked on:change={handleToggle}>
+    <input type="checkbox" class="fr-toggle__input" id="{toggleId}" bind:checked on:change={handleToggle}>
     <label class="fr-toggle__label" for="{toggleId}" data-fr-checked-label="Activé" data-fr-unchecked-label="Désactivé">Indicateur externe</label> 
 </div>
