b7ac57f9460c6a4659b2553985f8f790ecb23e71
Stéphane Blondon <stephane.blondon@gmail.com>
2023-01-27 19:19:47 +0100

    plus de champs d'entreprise transferés vers Metabase

--- impact/metabase/management/commands/sync_db.py
+++ impact/metabase/management/commands/sync_db.py
@@ -6,8 +6,11 @@ from metabase.models import Entreprise as MetabaseEntreprise
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for entreprise in ImpactEntreprise.objects.all():
-            meta_e = MetabaseEntreprise.objects.create()
-            meta_e.raison_sociale = entreprise.raison_sociale
-            meta_e.siren = entreprise.siren
+            meta_e = MetabaseEntreprise.objects.using("metabase").create(
+                siren=entreprise.siren,
+                raison_sociale=entreprise.raison_sociale,
+                bdese_accord=entreprise.bdese_accord,
+                effectif=entreprise.effectif,
+            )
             meta_e.save()
             self.stdout.write(self.style.SUCCESS(entreprise))
