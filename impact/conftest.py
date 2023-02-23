1a3ff5c4c276843f93b5cdd1c77783c15169ad87
Stéphane Blondon <stephane.blondon@gmail.com>
2023-02-23 13:35:48 +0100

    refacto: move a  fixture

--- impact/conftest.py
+++ impact/conftest.py
@@ -1,7 +1,26 @@
 import pytest
 
+from entreprises.models import Entreprise
 
 @pytest.fixture
 def alice(django_user_model):
     alice = django_user_model.objects.create(email="alice@impact.test")
     return alice
+
+@pytest.fixture
+def entreprise_factory(db):
+    def create_entreprise(
+        siren="000000001",
+        effectif="petit",
+        bdese_accord=False,
+        raison_sociale="Entreprise SAS",
+    ):
+        entreprise = Entreprise.objects.create(
+            siren=siren,
+            effectif=effectif,
+            bdese_accord=bdese_accord,
+            raison_sociale=raison_sociale,
+        )
+        return entreprise
+
+    return create_entreprise
