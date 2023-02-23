7d27762ef9de72a0b04aef032f0217d20cbf13e4
Stéphane Blondon <stephane.blondon@gmail.com>
2023-02-23 13:26:12 +0100

    ajout basique d'une entreprise par un utilisateur

--- impact/entreprises/tests/test_views.py
+++ impact/entreprises/tests/test_views.py
@@ -23,3 +23,16 @@ def test_entreprises_page_for_logged_user(client, django_user_model):
     assert response.status_code == 200
     content = response.content.decode("utf-8")
     assert "<!-- page entreprises -->" in content
+
+
+def test_add_and_attach_to_entreprise(client, alice, db):
+    client.force_login(alice)
+    data = {"siren": "130025265"}
+
+    response = client.post("/entreprises/add", data=data, follow=True)
+
+    assert response.status_code == 200
+    assert response.redirect_chain == [(reverse("entreprises"), 302)]
+    alice = User.objects.get(email="alice@impact.test")
+    entreprise = Entreprise.objects.get(siren="130025265")
+    assert entreprise in alice.entreprises
