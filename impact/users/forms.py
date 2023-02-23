600ce37d5db7a05a6f1887d776abc832ab611715
emillumine <camille@yaal.coop>
2023-02-23 18:14:26 +0100

    rend possible la modification de ses informations personnelles et mot de passe

--- impact/users/forms.py
+++ impact/users/forms.py
@@ -20,15 +20,51 @@ class LoginForm(DsfrForm, AuthenticationForm):
         self.fields["password"].label = "Mot de passe"
 
 
-class UserCreationForm(DsfrForm, forms.ModelForm):
+class UserPasswordForm(forms.ModelForm):
     password1 = forms.CharField(
         label="Mot de passe",
         widget=forms.PasswordInput,
         help_text=password_validation.password_validators_help_text_html(),
     )
     password2 = forms.CharField(
-        label="Confirmation du mot de passe", widget=forms.PasswordInput
+        label="Confirmation du mot de passe",
+        widget=forms.PasswordInput,
     )
+
+    class Meta:
+        model = User
+        fields = ("email",)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if (password1 or password2) and password1 != password2:
+            raise forms.ValidationError("Les mots de passe sont différents")
+        return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get("password2")
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except forms.ValidationError as error:
+                self.add_error("password1", error)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if password := self.cleaned_data.get("password1"):
+            # Save the provided password in hashed format
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
+
+class UserCreationForm(DsfrForm, UserPasswordForm):
     siren = forms.CharField(
         label="Numéro SIREN",
         help_text="Saisissez un numéro SIREN valide, disponible sur le Kbis de votre organisation ou sur l'Annuaire des Entreprises",
@@ -49,14 +85,6 @@ class UserCreationForm(DsfrForm, forms.ModelForm):
             "reception_actualites": "Je souhaite recevoir les actualités du projet Impact (optionnel)",
         }
 
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Les mots de passe sont différents")
-        return password2
-
     def clean_siren(self):
         cleaned_data = self.cleaned_data.get("siren")
         try:
@@ -65,20 +93,9 @@ class UserCreationForm(DsfrForm, forms.ModelForm):
             raise ValidationError("Le siren est incorrect")
         return cleaned_data
 
-    def _post_clean(self):
-        super()._post_clean()
-        # Validate the password after self.instance is updated with form data
-        # by super().
-        password = self.cleaned_data.get("password2")
-        if password:
-            try:
-                password_validation.validate_password(password, self.instance)
-            except forms.ValidationError as error:
-                self.add_error("password1", error)
-
     def save(self, commit=True):
-        # Save the provided password in hashed format
         user = super().save(commit=False)
+        # Save the provided password in hashed format
         user.set_password(self.cleaned_data["password1"])
         entreprise, _ = Entreprise.objects.get_or_create(
             siren=self.cleaned_data.get("siren")
@@ -92,6 +109,28 @@ class UserCreationForm(DsfrForm, forms.ModelForm):
             )
             entreprise.save()
         return user
+
+
+class UserEditionForm(DsfrForm, UserPasswordForm):
+    password1 = forms.CharField(
+        label="Mot de passe (optionnel)",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+    password2 = forms.CharField(
+        label="Confirmation du mot de passe (optionnel)",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = ("prenom", "nom", "email", "reception_actualites")
+        labels = {
+            "prenom": "Prénom",
+            "reception_actualites": "Je souhaite recevoir les actualités du projet Impact (optionnel)",
+        }
 
 
 class UserChangeForm(forms.ModelForm):
--- impact/users/tests/test_views.py
+++ impact/users/tests/test_views.py
@@ -51,6 +51,7 @@ def test_create_user_with_real_siren(reception_actualites, client, db):
         Habilitation.objects.get(user=user, entreprise=entreprise).fonctions
         == "Présidente"
     )
+    assert user.check_password("Passw0rd!123")
 
 
 def test_account_page_is_not_public(client):
@@ -67,3 +68,83 @@ def test_account_page_when_logged_in(client, alice):
     assert response.status_code == 200
     content = response.content.decode("utf-8")
     assert "<!-- page mon compte -->" in content, content
+
+
+@pytest.fixture
+def alice(django_user_model):
+    alice = django_user_model.objects.create(
+        prenom="Alice",
+        nom="Cooper",
+        email="alice@example.com",
+        reception_actualites=False,
+    )
+    alice.set_password("Passw0rd!123")
+    alice.save()  # il faut save après set_password
+    return alice
+
+
+def test_edit_account_info(client, alice):
+    client.force_login(alice)
+
+    data = {
+        "prenom": "Bob",
+        "nom": "Dylan",
+        "email": "bob@example.com",
+        "reception_actualites": "checked",
+    }
+
+    response = client.post("/mon-compte", data=data, follow=True)
+
+    assert response.status_code == 200
+    assert response.redirect_chain == [(reverse("account"), 302)]
+
+    content = response.content.decode("utf-8")
+    assert "Votre compte a bien été modifié." in content
+
+    alice.refresh_from_db()
+    assert alice.prenom == "Bob"
+    assert alice.nom == "Dylan"
+    assert alice.email == "bob@example.com"
+    assert alice.reception_actualites
+    assert alice.check_password("Passw0rd!123")
+
+
+def test_edit_account_info_and_password(client, alice):
+    client.force_login(alice)
+
+    response = client.get("/mon-compte")
+
+    assert response.status_code == 200
+
+    data = {
+        "prenom": "Bob",
+        "nom": "Dylan",
+        "email": "bob@example.com",
+        "reception_actualites": "checked",
+        "password1": "Yol0!1234567",
+        "password2": "Yol0!1234567",
+    }
+
+    response = client.post("/mon-compte", data=data, follow=True)
+
+    assert response.status_code == 200
+    assert response.redirect_chain == [
+        (reverse("account"), 302),
+        (
+            f"{reverse('login')}?next=/mon-compte",
+            302,
+        ),  # l'utilisateur doit se reconnecter
+    ]
+
+    content = response.content.decode("utf-8")
+    assert (
+        "Votre mot de passe a bien été modifié. Veuillez vous reconnecter." in content
+    )
+
+    user = User.objects.get(email="bob@example.com")
+    alice.refresh_from_db()
+    assert alice.prenom == "Bob"
+    assert alice.nom == "Dylan"
+    assert alice.email == "bob@example.com"
+    assert alice.reception_actualites
+    assert alice.check_password("Yol0!1234567")
