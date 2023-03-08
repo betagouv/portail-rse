3b308500bb69df81cdcedf2ab49ca9a9c80e50ac
emillumine <camille@yaal.coop>
2023-03-08 17:48:38 +0100

    sépare dans l'admin les BDESE personnelles et officielles

--- impact/reglementations/admin.py
+++ impact/reglementations/admin.py
@@ -1,7 +1,15 @@
 from django import forms
 from django.contrib import admin
 
-from .models import BDESE_300, BDESE_50_300, CategoryType
+from reglementations.models import (
+    BDESE_300,
+    BDESE_50_300,
+    CategoryType,
+    OfficialBDESE_300,
+    OfficialBDESE_50_300,
+    PersonalBDESE_300,
+    PersonalBDESE_50_300,
+)
 
 
 class CategoryJSONField(forms.JSONField):
@@ -16,33 +24,24 @@ class CategoryJSONField(forms.JSONField):
         super().__init__(*args, **kwargs)
 
 
-class BDESE_300Form(forms.ModelForm):
+class BDESEAdminForm(forms.ModelForm):
     class Meta:
-        model = BDESE_300
         fields = "__all__"
         field_classes = {
             category_field: CategoryJSONField
             for category_field in BDESE_300.category_fields()
+            + BDESE_50_300.category_fields()
         }
 
 
-class BDESE_50_300Form(forms.ModelForm):
-    class Meta:
-        model = BDESE_50_300
-        fields = "__all__"
-        field_classes = {
-            category_field: CategoryJSONField
-            for category_field in BDESE_50_300.category_fields()
-        }
+@admin.register(PersonalBDESE_300, PersonalBDESE_50_300)
+class PersonalBDESE_Admin(admin.ModelAdmin):
+    form = BDESEAdminForm
+    list_display = ["entreprise", "annee", "user"]
+    list_filter = (("user", admin.RelatedOnlyFieldListFilter),)
 
 
-@admin.register(BDESE_300)
-class BDESE_300Admin(admin.ModelAdmin):
-    form = BDESE_300Form
-    list_display = ["entreprise", "annee"]
-
-
-@admin.register(BDESE_50_300)
-class BDESE_50_300Admin(admin.ModelAdmin):
-    form = BDESE_50_300Form
+@admin.register(OfficialBDESE_300, OfficialBDESE_50_300)
+class OfficialBDESE_Admin(admin.ModelAdmin):
+    form = BDESEAdminForm
     list_display = ["entreprise", "annee"]
