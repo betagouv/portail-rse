a0005cf8b033af0ccd7be5d8c5615a8ded65c327
Stéphane Blondon <stephane.blondon@gmail.com>
2023-03-14 15:49:07 +0100

    fix: un message non dupliqué refacto: une seule route pour toutes les étapes de la BDESE

--- impact/reglementations/urls.py
+++ impact/reglementations/urls.py
@@ -5,11 +5,6 @@ from . import views
 urlpatterns = [
     path("reglementations", views.reglementations, name="reglementations"),
     path("reglementation/<str:siren>", views.reglementation, name="reglementation"),
-    path(
-        "bdese/<str:siren>/<int:annee>/0",
-        views.bdese_configuration,
-        name="bdese_configuration",
-    ),
     path("bdese/<str:siren>/<int:annee>/<int:step>", views.bdese, name="bdese"),
     path("bdese/<str:siren>/<int:annee>/pdf", views.bdese_pdf, name="bdese_pdf"),
 ]
