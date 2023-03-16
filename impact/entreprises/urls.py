1d92c451aa58deb4c14a5fb8881a25df617502c3
emillumine <camille@yaal.coop>
2023-03-16 10:13:54 +0100

    refacto: renomme detail_entreprise en qualification

--- impact/entreprises/urls.py
+++ impact/entreprises/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("entreprises", views.index, name="entreprises"),
     path("entreprises/add", views.add, name="add_entreprise"),
-    path("entreprises/<str:siren>", views.detail_entreprise, name="detail_entreprise"),
+    path("entreprises/<str:siren>", views.qualification, name="qualification"),
     path("api/search-entreprise/<str:siren>", views.search_entreprise),
 ]
