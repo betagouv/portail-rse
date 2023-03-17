5da39e45c1935a714c4279a187dade3aed6f043d
emillumine <camille@yaal.coop>
2023-03-17 12:37:01 +0100

    rend la raison sociale d'une Entreprise obligatoire et l'effectif facultatif

--- impact/entreprises/admin.py
+++ impact/entreprises/admin.py
@@ -5,9 +5,17 @@ from entreprises.models import Entreprise
 from habilitations.admin import HabilitationInline
 
 
+class EntrepriseAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["effectif"].required = False
+
+
 class EntrepriseAdmin(admin.ModelAdmin):
     list_display = ["siren", "raison_sociale"]
     inlines = (HabilitationInline,)
+    model = Entreprise
+    form = EntrepriseAdminForm
 
 
 admin.site.register(Entreprise, EntrepriseAdmin)
--- impact/entreprises/models.py
+++ impact/entreprises/models.py
@@ -19,6 +19,7 @@ class Entreprise(TimestampedModel):
     ]
 
     siren = models.CharField(max_length=9, unique=True)
+    raison_sociale = models.CharField(max_length=RAISON_SOCIALE_MAX_LENGTH)
     users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, through="habilitations.Habilitation"
     )
@@ -26,12 +27,12 @@ class Entreprise(TimestampedModel):
         max_length=9,
         choices=EFFECTIF_CHOICES,
         help_text="Vérifiez et confirmez le nombre de salariés",
+        null=True,
     )
     bdese_accord = models.BooleanField(
         verbose_name="L'entreprise a un accord collectif d'entreprise concernant la Base de Données Économiques, Sociales et Environnementales (BDESE)",
         default=False,
     )
-    raison_sociale = models.CharField(max_length=RAISON_SOCIALE_MAX_LENGTH, default="")
 
     def __str__(self):
         return f"{self.siren} {self.raison_sociale}"
