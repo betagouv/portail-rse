f5b4bbe7428ca197a3da37c0c77808e4a348b939
Stéphane Blondon <stephane.blondon@gmail.com>
2023-04-14 17:37:14 +0200

    nettoyage: suppression de code commenté

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,7 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte({compilerOptions: {hydratable: true}})],
-  // plugins: [svelte()],
   base: "/static/svelte",
   build: {
     outDir: "../impact/static/svelte",
