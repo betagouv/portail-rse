d1451d3a1a5500d6d9e4d5aa4f8d77b9bc08aa6a
emillumine <camille@yaal.coop>
2023-05-12 18:11:01 +0200

    désactive le bouton submit du formulaire tant qu'une entreprise n'est pas trouvée

--- front/src/lib/SearchEntreprise.svelte
+++ front/src/lib/SearchEntreprise.svelte
@@ -1,47 +1,52 @@
 <script>
     import spinner from './assets/spinner.svg';
+
     let siren = ""
-    let recherche = false
+    let loading = false
     let promise = async () => {}
 
+    const sirenFieldId = "id_siren" // defined by django
+    const submitButton = document.getElementById(sirenFieldId).closest("form").querySelector("[type=submit]")
+    submitButton.disabled = true
+
     async function searchEntreprise(siren) {
-        if( siren.length !== 9){
-            recherche = false
+        if( siren.length !== 9 || isNaN(siren)){
             throw new Error("Le siren est incorrect.")
         }
+        loading = true
         const res = await fetch("/api/search-entreprise/" + siren)
         const json = await res.json();
 
         if (res.ok) {
-            recherche = false
+            loading = false
+            submitButton.disabled = false
             return json;
         } else {
-            recherche = false
+            loading = false
             throw new Error(json['error'])
         }
     }
 
     const handleChange = () => {
-        recherche = true
         promise = searchEntreprise(siren)
     }
 </script>
 
-<fieldset class="fr-fieldset fr-mb-3w" aria-label="SIREN de l'entreprise">
-    <div class="fr-fieldset__element fr-mb-2w">
+<fieldset class="fr-fieldset" aria-label="SIREN de l'entreprise">
+    <div class="fr-fieldset__element">
         <div class="fr-input-group">
-            <label class="fr-label" for="id_siren">Votre numéro SIREN
+            <label class="fr-label" for="{sirenFieldId}">Votre numéro SIREN
                 <span class="fr-hint-text">Saisissez un numéro SIREN valide, disponible sur le Kbis de votre organisation ou sur l'Annuaire des Entreprises</span>
             </label>
-            <div class="fr-col-12 fr-col-sm-6">
+            <div class="fr-col-12 fr-col-sm-6 fr-mt-1w">
                 <div class="fr-search-bar" role="search">
-                {#if ! recherche}
-                    <input type="search" name="siren" maxlength="9" minlength="9" class="fr-input" id="id_siren" required bind:value={siren} on:change|preventDefault={handleChange}>
-                    <button class="fr-btn" title="Rechercher" on:click|preventDefault={handleChange}>
+                {#if ! loading}
+                    <input type="search" name="siren" maxlength="9" minlength="9" class="fr-input" id="{sirenFieldId}" required bind:value={siren} on:change|preventDefault={handleChange}>
+                    <button type="button" class="fr-btn" title="Rechercher" on:click|preventDefault={handleChange}>
                         Rechercher
                     </button>
                 {:else}
-                    <input type="text" name="siren" maxlength="9" minlength="9" class="fr-input" id="id_siren" value="{siren}" readonly>
+                    <input type="text" name="siren" maxlength="9" minlength="9" class="fr-input" id="{sirenFieldId}" value="{siren}" readonly>
                     <img src="{spinner}" width="40" alt="Spinner d'attente">
                 {/if}
                 </div>
