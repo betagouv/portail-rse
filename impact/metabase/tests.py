d2e5567eb510cf08067cc01df2d7fc2a0d2cf4c5
Stéphane Blondon <stephane.blondon@gmail.com>
2023-06-12 18:39:26 +0200

    refacto: réutilisation d'une fixture

--- impact/metabase/tests.py
+++ impact/metabase/tests.py
@@ -1,21 +1,16 @@
 import pytest
 
-from entreprises.models import Entreprise as ImpactEntreprise
 from entreprises.models import Evolution
-from entreprises.models import set_current_evolution
 from impact.settings import METABASE_DATABASE_NAME
 from metabase.management.commands.sync_metabase import Command
 from metabase.models import Entreprise as MetabaseEntreprise
 
 
 @pytest.mark.django_db(transaction=True, databases=["default", METABASE_DATABASE_NAME])
-def test_synchronise_metabase_once():
-    entreprise_A = ImpactEntreprise.objects.create(
+def test_synchronise_metabase_once(entreprise_factory):
+    entreprise_A = entreprise_factory(
         siren="000000001",
         denomination="A",
-    )
-    set_current_evolution(
-        entreprise=entreprise_A,
         effectif=Evolution.EFFECTIF_ENTRE_300_ET_499,
         bdese_accord=True,
     )
@@ -32,13 +27,10 @@ def test_synchronise_metabase_once():
 
 
 @pytest.mark.django_db(transaction=True, databases=["default", METABASE_DATABASE_NAME])
-def test_synchronise_several_times():
-    entreprise_A = ImpactEntreprise.objects.create(
+def test_synchronise_several_times(entreprise_factory):
+    entreprise_A = entreprise_factory(
         siren="000000001",
         denomination="A",
-    )
-    set_current_evolution(
-        entreprise=entreprise_A,
         effectif=Evolution.EFFECTIF_MOINS_DE_50,
         bdese_accord=True,
     )
