5f07e4a05cb58ef83a6f6d0a34c27730966ed8b0
Stéphane Blondon <stephane.blondon@gmail.com>
2023-08-08 17:37:16 +0200

    affichage des champs CA et bilan consolidés uniquement si la holding a des comptes consolidés

--- front/src/lib/GroupeFields.svelte
+++ front/src/lib/GroupeFields.svelte
@@ -1,11 +1,19 @@
 <script>
     const appartientGroupeId = "id_appartient_groupe" // defined by django
+    const etablitComptesConsolidesId = "id_comptes_consolides" // defined by django
 
     const groupeFieldset = document.getElementById("svelte-groupe-fields")
     const appartientGroupeField = document.getElementById(appartientGroupeId)
 
+    const comptesConsolidesFields = document.getElementById("svelte-comptes-consolides-fields")
+    const etablitComptesConsolidesField = document.getElementById(etablitComptesConsolidesId)
+
     const displayGroupeFields = () => {
         groupeFieldset.style.display = appartientGroupeField.checked ? "block" : "none"
+    }
+
+    const displayComptesConsolidesFields = () => {
+      comptesConsolidesFields.style.display = etablitComptesConsolidesField.checked ? "block" : "none"
     }
 
     appartientGroupeField.addEventListener(
@@ -13,6 +21,12 @@
       displayGroupeFields,
     );
 
+    etablitComptesConsolidesField.addEventListener(
+      "change",
+      displayComptesConsolidesFields,
+    );
+
     displayGroupeFields()
+    displayComptesConsolidesFields()
 
 </script>
