e497c54f0df131a056e04789251de0ad715f8255
emillumine <camille@yaal.coop>
2023-08-23 12:15:42 +0200

    affiche le bouton de consultation des bilans GES quand on n'est pas soumis même si on n'est pas authentifié

--- impact/reglementations/tests/test_bges.py
+++ impact/reglementations/tests/test_bges.py
@@ -1,4 +1,6 @@
 import pytest
+from django.contrib.auth.models import AnonymousUser
+from django.urls import reverse
 
 from entreprises.models import CaracteristiquesAnnuelles
 from habilitations.models import attach_user_to_entreprise
@@ -16,6 +18,76 @@ def test_bges_reglementation_info():
         == "Le Bilan GES réglementaire a vocation à contribuer à la mise en œuvre de la stratégie de réduction des émissions de GES des entreprises. Un plan de transition est obligatoirement joint à ce bilan. Il vise à réduire les émissions de gaz à effet de serre et présente les objectifs, moyens et actions envisagées à cette fin ainsi que, le cas échéant, les actions mises en œuvre lors du précédent bilan. Ils sont mis à jour tous les quatre ans."
     )
     assert info["more_info_url"] == "https://bilans-ges.ademe.fr/"
+
+
+def test_calculate_status_with_not_authenticated_user(entreprise_factory, mocker):
+    entreprise = entreprise_factory()
+
+    mocker.patch(
+        "reglementations.views.bges.BGESReglementation.est_soumis",
+        return_value=False,
+    )
+    status = BGESReglementation(entreprise).calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, AnonymousUser()
+    )
+
+    assert status.status == ReglementationStatus.STATUS_NON_SOUMIS
+    assert status.status_detail == "Vous n'êtes pas soumis à cette réglementation."
+    assert status.primary_action.url == "https://bilans-ges.ademe.fr/bilans"
+    assert status.primary_action.title == "Consulter les Bilans GES sur Bilans GES"
+    assert status.primary_action.external
+    assert status.secondary_actions == []
+
+    mocker.patch(
+        "reglementations.views.bges.BGESReglementation.est_soumis",
+        return_value=True,
+    )
+    status = BGESReglementation(entreprise).calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, AnonymousUser()
+    )
+
+    assert status.status == ReglementationStatus.STATUS_SOUMIS
+    login_url = f"{reverse('users:login')}?next={reverse('reglementations:reglementations', args=[entreprise.siren])}"
+
+    assert (
+        status.status_detail
+        == f'<a href="{login_url}">Vous êtes soumis à cette réglementation. Connectez-vous pour en savoir plus.</a>'
+    )
+    assert status.primary_action.title == "Se connecter"
+    assert status.primary_action.url == login_url
+    assert status.secondary_actions == []
+
+
+def test_calculate_status_with_not_attached_user(entreprise_factory, alice, mocker):
+    entreprise = entreprise_factory()
+
+    mocker.patch(
+        "reglementations.views.bges.BGESReglementation.est_soumis",
+        return_value=False,
+    )
+    status = BGESReglementation(entreprise).calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, alice
+    )
+
+    assert status.status == ReglementationStatus.STATUS_NON_SOUMIS
+    assert (
+        status.status_detail == "L'entreprise n'est pas soumise à cette réglementation."
+    )
+    assert status.primary_action is None
+    assert status.secondary_actions == []
+
+    mocker.patch(
+        "reglementations.views.bges.BGESReglementation.est_soumis",
+        return_value=True,
+    )
+    status = BGESReglementation(entreprise).calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, alice
+    )
+
+    assert status.status == ReglementationStatus.STATUS_SOUMIS
+    assert status.status_detail == "L'entreprise est soumise à cette réglementation."
+    assert status.primary_action is None
+    assert status.secondary_actions == []
 
 
 @pytest.mark.parametrize(
