fd04764da76cf7acc638f8d77327c280d30876e4
emillumine <camille@yaal.coop>
2023-08-23 11:53:41 +0200

    refacto: extrait des méthodes

--- impact/reglementations/views/base.py
+++ impact/reglementations/views/base.py
@@ -59,23 +59,33 @@ class Reglementation(ABC):
         user: settings.AUTH_USER_MODEL,
     ) -> ReglementationStatus:
         if not user.is_authenticated:
-            if self.est_soumis(caracteristiques):
-                status = ReglementationStatus.STATUS_SOUMIS
-                login_url = f"{reverse_lazy('users:login')}?next={reverse_lazy('reglementations:reglementations', args=[self.entreprise.siren])}"
-                status_detail = f'<a href="{login_url}">Vous êtes soumis à cette réglementation. Connectez-vous pour en savoir plus.</a>'
-                primary_action = ReglementationAction(login_url, f"Se connecter")
-            else:
-                status = ReglementationStatus.STATUS_NON_SOUMIS
-                status_detail = "Vous n'êtes pas soumis à cette réglementation."
-                primary_action = None
-            return ReglementationStatus(
-                status, status_detail, primary_action=primary_action
-            )
+            return self.calculate_status_for_anonymous_user(caracteristiques)
         elif not is_user_attached_to_entreprise(user, self.entreprise):
-            if self.est_soumis(caracteristiques):
-                status = ReglementationStatus.STATUS_SOUMIS
-                status_detail = "L'entreprise est soumise à cette réglementation."
-            else:
-                status = ReglementationStatus.STATUS_NON_SOUMIS
-                status_detail = "L'entreprise n'est pas soumise à cette réglementation."
-            return ReglementationStatus(status, status_detail)
+            return self.calculate_status_for_unauthorized_user(caracteristiques)
+
+    def calculate_status_for_anonymous_user(
+        self, caracteristiques: CaracteristiquesAnnuelles
+    ):
+        if self.est_soumis(caracteristiques):
+            status = ReglementationStatus.STATUS_SOUMIS
+            login_url = f"{reverse_lazy('users:login')}?next={reverse_lazy('reglementations:reglementations', args=[self.entreprise.siren])}"
+            status_detail = f'<a href="{login_url}">Vous êtes soumis à cette réglementation. Connectez-vous pour en savoir plus.</a>'
+            primary_action = ReglementationAction(login_url, f"Se connecter")
+        else:
+            status = ReglementationStatus.STATUS_NON_SOUMIS
+            status_detail = "Vous n'êtes pas soumis à cette réglementation."
+            primary_action = None
+        return ReglementationStatus(
+            status, status_detail, primary_action=primary_action
+        )
+
+    def calculate_status_for_unauthorized_user(
+        self, caracteristiques: CaracteristiquesAnnuelles
+    ):
+        if self.est_soumis(caracteristiques):
+            status = ReglementationStatus.STATUS_SOUMIS
+            status_detail = "L'entreprise est soumise à cette réglementation."
+        else:
+            status = ReglementationStatus.STATUS_NON_SOUMIS
+            status_detail = "L'entreprise n'est pas soumise à cette réglementation."
+        return ReglementationStatus(status, status_detail)
