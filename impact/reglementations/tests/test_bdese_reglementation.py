c36fc297492d6d796945e96ca8bd4dc9e6c848bb
emillumine <camille@yaal.coop>
2023-09-01 18:09:35 +0200

    supprime le bouton de connexion sur les réglementations de la simulation quand on est soumis refactorise des tests

--- impact/reglementations/tests/test_bdese_reglementation.py
+++ impact/reglementations/tests/test_bdese_reglementation.py
@@ -31,70 +31,59 @@ def test_bdese_reglementation_info():
     )
 
 
-def test_calculate_status_with_not_authenticated_user(entreprise_factory, mocker):
+@pytest.mark.parametrize("est_soumis", [True, False])
+def test_calculate_status_with_not_authenticated_user(
+    est_soumis, entreprise_factory, mocker
+):
     entreprise = entreprise_factory()
-
-    mocker.patch(
-        "reglementations.views.bdese.BDESEReglementation.est_soumis",
-        return_value=False,
-    )
-    status = BDESEReglementation.calculate_status(
-        entreprise.dernieres_caracteristiques_qualifiantes, AnonymousUser()
-    )
-
-    assert status.status == ReglementationStatus.STATUS_NON_SOUMIS
-    assert status.status_detail == "Vous n'êtes pas soumis à cette réglementation."
-    assert status.primary_action is None
-    assert status.secondary_actions == []
-
-    mocker.patch(
-        "reglementations.views.bdese.BDESEReglementation.est_soumis",
-        return_value=True,
-    )
-    status = BDESEReglementation.calculate_status(
-        entreprise.dernieres_caracteristiques_qualifiantes, AnonymousUser()
-    )
-
-    assert status.status == ReglementationStatus.STATUS_SOUMIS
     login_url = f"{reverse('users:login')}?next={reverse('reglementations:reglementations', args=[entreprise.siren])}"
 
-    assert (
-        status.status_detail
-        == f'<a href="{login_url}">Vous êtes soumis à cette réglementation. Connectez-vous pour en savoir plus.</a>'
+    mocker.patch(
+        "reglementations.views.bdese.BDESEReglementation.est_soumis",
+        return_value=est_soumis,
     )
-    assert status.primary_action.title == "Se connecter"
-    assert status.primary_action.url == login_url
+    status = BDESEReglementation.calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, AnonymousUser()
+    )
+
+    if est_soumis:
+        assert status.status == ReglementationStatus.STATUS_SOUMIS
+        assert (
+            status.status_detail
+            == f'<a href="{login_url}">Vous êtes soumis à cette réglementation. Connectez-vous pour en savoir plus.</a>'
+        )
+    else:
+        assert status.status == ReglementationStatus.STATUS_NON_SOUMIS
+        assert status.status_detail == "Vous n'êtes pas soumis à cette réglementation."
+    assert status.primary_action is None
     assert status.secondary_actions == []
 
 
-def test_calculate_status_with_not_attached_user(entreprise_factory, alice, mocker):
+@pytest.mark.parametrize("est_soumis", [True, False])
+def test_calculate_status_with_not_attached_user(
+    est_soumis, entreprise_factory, alice, mocker
+):
     entreprise = entreprise_factory()
 
     mocker.patch(
         "reglementations.views.bdese.BDESEReglementation.est_soumis",
-        return_value=False,
+        return_value=est_soumis,
     )
     status = BDESEReglementation.calculate_status(
         entreprise.dernieres_caracteristiques_qualifiantes, alice
     )
 
-    assert status.status == ReglementationStatus.STATUS_NON_SOUMIS
-    assert (
-        status.status_detail == "L'entreprise n'est pas soumise à cette réglementation."
-    )
-    assert status.primary_action is None
-    assert status.secondary_actions == []
-
-    mocker.patch(
-        "reglementations.views.bdese.BDESEReglementation.est_soumis",
-        return_value=True,
-    )
-    status = BDESEReglementation.calculate_status(
-        entreprise.dernieres_caracteristiques_qualifiantes, alice
-    )
-
-    assert status.status == ReglementationStatus.STATUS_SOUMIS
-    assert status.status_detail == "L'entreprise est soumise à cette réglementation."
+    if est_soumis:
+        assert status.status == ReglementationStatus.STATUS_SOUMIS
+        assert (
+            status.status_detail == "L'entreprise est soumise à cette réglementation."
+        )
+    else:
+        assert status.status == ReglementationStatus.STATUS_NON_SOUMIS
+        assert (
+            status.status_detail
+            == "L'entreprise n'est pas soumise à cette réglementation."
+        )
     assert status.primary_action is None
     assert status.secondary_actions == []
 
