68fee4f42c12221443b5a4c602ebe5b41ea7a457
emillumine <camille@yaal.coop>
2023-09-05 14:55:42 +0200

    déplace le lancement des migrations dans un hook post deploy comme recommandé par Scalingo lors du déploiement il y a un timeout sur le lancement du conteneur au bout de 60s ce qui n'est pas assez pour jouer certaines migrations le timeout du postdeploy est supérieur (20min) ce qui devrait régler le problème https://doc.scalingo.com/platform/app/postdeploy-hook#limits

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bash start.sh
+postdeploy: python impact/manage.py migrate && python impact/manage.py migrate metabase --database=metabase
--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 python impact/manage.py collectstatic --no-input
-python impact/manage.py migrate
-python impact/manage.py migrate metabase --database=metabase
 
 gunicorn --pythonpath=impact impact.wsgi --log-file -
