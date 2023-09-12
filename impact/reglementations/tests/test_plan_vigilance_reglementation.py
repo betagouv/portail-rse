a5fbc0cdd63a0e7f69cfcc256bf20667e4f90f6a
emillumine <camille@yaal.coop>
2023-09-12 17:07:59 +0200

    commence l'implémentation du plan de vigilance

---
+++ impact/reglementations/tests/test_plan_vigilance_reglementation.py
@@ -0,0 +1,86 @@
+import pytest
+
+from entreprises.models import CaracteristiquesAnnuelles
+from habilitations.models import attach_user_to_entreprise
+from reglementations.views.base import ReglementationStatus
+from reglementations.views.plan_vigilance import PlanVigilanceReglementation
+
+
+def test_reglementation_info():
+    info = PlanVigilanceReglementation.info()
+
+    assert info["title"] == "Plan de vigilance"
+
+    assert (
+        info["description"]
+        == """Le plan de vigilance comporte les mesures de vigilance propres à identifier et à prévenir les atteintes graves envers les droits humains et les libertés fondamentales,
+        la santé et la sécurité des personnes ainsi que de l’environnement qui adviendraient au sein de l’entreprise."""
+    )
+    assert not info["more_info_url"]
+    assert info["tag"] == "tag-social"
+
+
+@pytest.mark.parametrize(
+    "effectif",
+    [
+        CaracteristiquesAnnuelles.EFFECTIF_MOINS_DE_50,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_50_ET_249,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_250_ET_299,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_300_ET_499,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_500_ET_4999,
+    ],
+)
+@pytest.mark.parametrize(
+    "effectif_groupe",
+    [
+        None,
+        CaracteristiquesAnnuelles.EFFECTIF_MOINS_DE_50,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_50_ET_249,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_250_ET_499,
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_500_ET_4999,
+    ],
+)
+def test_calcule_statut_moins_de_5000_employes(
+    effectif, effectif_groupe, entreprise_factory, alice
+):
+    entreprise = entreprise_factory(
+        effectif=effectif,
+        appartient_groupe=True if effectif_groupe else False,
+        effectif_groupe=effectif_groupe,
+    )
+    attach_user_to_entreprise(alice, entreprise, "Présidente")
+
+    reglementation = PlanVigilanceReglementation.calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, alice
+    )
+
+    assert reglementation.status == ReglementationStatus.STATUS_NON_SOUMIS
+    assert (
+        reglementation.status_detail == "Vous n'êtes pas soumis à cette réglementation."
+    )
+    assert not reglementation.primary_action
+
+
+@pytest.mark.parametrize(
+    "effectif",
+    [
+        CaracteristiquesAnnuelles.EFFECTIF_ENTRE_5000_ET_9999,
+        CaracteristiquesAnnuelles.EFFECTIF_10000_ET_PLUS,
+    ],
+)
+def test_calcule_statut_plus_de_5000_employes(effectif, entreprise_factory, alice):
+    entreprise = entreprise_factory(effectif=effectif)
+    attach_user_to_entreprise(alice, entreprise, "Présidente")
+
+    reglementation = PlanVigilanceReglementation.calculate_status(
+        entreprise.dernieres_caracteristiques_qualifiantes, alice
+    )
+
+    assert reglementation.status == ReglementationStatus.STATUS_SOUMIS
+    assert reglementation.status_detail.startswith(
+        "Vous êtes soumis à cette réglementation car votre effectif est supérieur à 5000 salariés."
+    )
+    assert reglementation.status_detail.endswith(
+        "Vous devez établir un plan de vigilance si vous employez, à la clôture de deux exercices consécutifs, au moins 5 000 salariés, en votre sein ou dans vos filiales directes ou indirectes françaises, ou 10 000 salariés, en incluant vos filiales directes ou indirectes étrangères."
+    )
+    assert not reglementation.primary_action
