930f667b6cf514f93cd6a06445f8ad6c541be6fd
Stéphane <stephane@yaal.coop>
2023-09-22 17:43:32 +0200

    suppression de l'étape 1 de la simulation

--- front/src/lib/SimulationForm.svelte
+++ front/src/lib/SimulationForm.svelte
@@ -1,5 +1,9 @@
 <script>
+    import spinner from './assets/spinner.svg';
+
     export let csrfToken = undefined;
+    export let siren = "";
+    export let denomination = "";
     export let effectif = "";
     export let trancheBilan = "";
     export let trancheChiffreAffaires = "";
@@ -8,12 +12,76 @@
     export let trancheChiffreAffairesConsolide = "";
     export let appartientGroupe = false;
     export let comptesConsolides = false;
-</script>
+
+    let loading = false
+    let promise = async () => {}
+
+    const sirenFieldId = "id_siren" // defined by django
+
+    async function searchEntreprise(siren) {
+        if( siren.length !== 9 || isNaN(siren)){
+            throw new Error("Le siren est incorrect.")
+        }
+        loading = true
+        const res = await fetch("/api/search-entreprise/" + siren)
+        const json = await res.json();
+
+        if (res.ok) {
+            loading = false
+            denomination = json.denomination
+            return json;
+        } else {
+            loading = false
+            throw new Error(json['error'])
+        }
+    }
+
+    const handleChange = () => {
+        denomination = ""
+        promise = searchEntreprise(siren)
+    }
+    </script>
 
 <form class="fr-mt-6w" action="/reglementations" method="post">
+<fieldset class="fr-fieldset" aria-label="SIREN de l'entreprise">
+    <div class="fr-fieldset__element">
+        <div class="fr-input-group">
+            <label class="fr-label" for="{sirenFieldId}">Votre numéro SIREN
+                <span class="fr-hint-text">Saisissez un numéro SIREN valide, disponible sur le Kbis de votre organisation ou sur l'Annuaire des Entreprises</span>
+            </label>
+            <div class="fr-col-12 fr-col-sm-6 fr-mt-1w">
+                <div class="fr-search-bar" role="search">
+                {#if ! loading}
+                    <input type="search" name="siren" maxlength="9" minlength="9" class="fr-input" id="{sirenFieldId}" required bind:value={siren} on:change|preventDefault={handleChange}>
+                    <button type="button" class="fr-btn" title="Rechercher" on:click|preventDefault={handleChange}>
+                        Rechercher
+                    </button>
+                {:else}
+                    <input type="text" name="siren" maxlength="9" minlength="9" class="fr-input" id="{sirenFieldId}" value="{siren}" readonly>
+                    <img src="{spinner}" width="40" alt="Spinner d'attente">
+                {/if}
+                </div>
+            </div>
+            {#await promise then json}
+                {#if json.denomination}
+                    <p class="fr-mt-1w fr-mb-n1v">Entreprise : {json.denomination}</p>
+                {/if}
+            {:catch error}
+                <p class="fr-error-text">{error.message}</p>
+            {/await}
+        </div>
+    </div>
+    <div class="fr-fieldset__element">
+        <a class="fr-link" target="_blank" rel="noopener noreferrer" href="https://annuaire-entreprises.data.gouv.fr/">
+            Trouvez votre SIREN sur l'Annuaire des entreprises
+        </a>
+    </div>
+</fieldset>
+
+
+{#if denomination}
     <input type="hidden" name="csrfmiddlewaretoken" value="{csrfToken}">
-    <input type="hidden" name="siren" value="889297453" id="id_siren">
-    <input type="hidden" name="denomination" value="YAAL COOP" id="id_denomination">
+    <input type="hidden" name="denomination" value="{denomination}" id="id_denomination">
 
     <div class="fr-select-group">
         <label class="fr-label" for="id_effectif">Effectif total
@@ -163,4 +231,5 @@
         {/if}
     </fieldset>
     <input type="submit" value="Vérifier mes obligations" class="fr-btn">
+{/if}
 </form>
