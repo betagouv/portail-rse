ff88af6a324a92c932a4474e3b3dd19ff2253299
emillumine <camille@yaal.coop>
2023-09-25 10:43:59 +0200

    refacto: variabilise les id de tous les champs

--- front/src/lib/SimulationForm.svelte
+++ front/src/lib/SimulationForm.svelte
@@ -1,22 +1,33 @@
 <script>
-    import spinner from './assets/spinner.svg';
+    import spinner from './assets/spinner.svg'
 
-    export let csrfToken = undefined;
-    export let siren = "";
-    export let denomination = "";
-    export let effectif = "";
-    export let trancheBilan = "";
-    export let trancheChiffreAffaires = "";
-    export let effectifGroupe = "";
-    export let trancheBilanConsolide = "";
-    export let trancheChiffreAffairesConsolide = "";
-    export let appartientGroupe = false;
-    export let comptesConsolides = false;
+    export let csrfToken = undefined
+    export let siren = ""
+    export let denomination = ""
+    export let effectif = ""
+    export let trancheBilan = ""
+    export let trancheChiffreAffaires = ""
+    export let effectifGroupe = ""
+    export let trancheBilanConsolide = ""
+    export let trancheChiffreAffairesConsolide = ""
+    export let appartientGroupe = false
+    export let comptesConsolides = false
 
     let loading = false
     let promise = async () => {}
 
-    const sirenFieldId = "id_siren" // defined by django
+    // defined by Django
+    const sirenFieldId = "id_siren"
+    const csrfTokenFieldName = "csrfmiddlewaretoken"
+    const denominationFieldId = "id_denomination"
+    const effectifFieldId = "id_effectif"
+    const trancheBilanFieldId = "id_tranche_bilan"
+    const trancheChiffreAffairesFieldId = "id_tranche_chiffre_affaires"
+    const effectifGroupeFieldId = "id_effectif_groupe"
+    const trancheBilanConsolideFieldId = "id_tranche_bilan_consolide"
+    const trancheChiffreAffairesConsolideFieldId = "id_tranche_chiffre_affaires_consolide"
+    const appartientGroupeFieldId = "id_appartient_groupe"
+    const comptesConsolidesFieldId = "id_comptes_consolides"
 
     async function searchEntreprise(siren) {
         if( siren.length !== 9 || isNaN(siren)){
@@ -40,7 +51,7 @@
         denomination = ""
         promise = searchEntreprise(siren)
     }
-    </script>
+</script>
 
 <form class="fr-mt-6w" action="/reglementations" method="post">
 <fieldset class="fr-fieldset" aria-label="SIREN de l'entreprise">
@@ -78,17 +89,16 @@
     </div>
 </fieldset>
 
-
 {#if denomination}
-    <input type="hidden" name="csrfmiddlewaretoken" value="{csrfToken}">
-    <input type="hidden" name="denomination" value="{denomination}" id="id_denomination">
+    <input type="hidden" name="{csrfTokenFieldName}" value="{csrfToken}">
+    <input type="hidden" name="denomination" value="{denomination}" id="{denominationFieldId}">
 
     <div class="fr-select-group">
-        <label class="fr-label" for="id_effectif">Effectif
+        <label class="fr-label" for="{effectifFieldId}">Effectif
             <span class="fr-hint-text">Vérifiez et confirmez le nombre de salariés</span>
         </label>
         <div class="fr-col-12 ">
-            <select name="effectif" class="fr-select" required id="id_effectif"
+            <select name="effectif" class="fr-select" required id="{effectifFieldId}"
                 bind:value={effectif}
             >
                 <option value="">---------</option>
@@ -104,11 +114,11 @@
     </div>
 
     <div class="fr-select-group">
-        <label class="fr-label" for="id_tranche_chiffre_affaires">Chiffre d&#x27;affaires
+        <label class="fr-label" for="{trancheChiffreAffairesFieldId}">Chiffre d&#x27;affaires
             <span class="fr-hint-text">Montant net du chiffre d'affaires de l'exercice clos</span>
         </label>
         <div class="fr-col-12 ">
-            <select name="tranche_chiffre_affaires" class="fr-select" required id="id_tranche_chiffre_affaires"
+            <select name="tranche_chiffre_affaires" class="fr-select" required id="{trancheChiffreAffairesFieldId}"
                 bind:value={trancheChiffreAffaires}
             >
                 <option value="">---------</option>
@@ -123,14 +133,13 @@
     </div>
 
     <div class="fr-select-group">
-        <label class="fr-label" for="id_tranche_bilan">Bilan
+        <label class="fr-label" for="{trancheBilanFieldId}">Bilan
             <span class="fr-hint-text">Total du bilan de l'exercice clos</span>
         </label>
         <div class="fr-col-12 ">
-            <select name="tranche_bilan" class="fr-select" required id="id_tranche_bilan"
+            <select name="tranche_bilan" class="fr-select" required id="{trancheBilanFieldId}"
                 bind:value={trancheBilan}
             >
-
                 <option value="">---------</option>
                 <option value="0-350k">moins de 350k€</option>
                 <option value="350k-6M">entre 350k€ et 6M€</option>
@@ -146,22 +155,21 @@
         <legend class="fr-fieldset__legend">Groupe d'entreprises</legend>
         <div class="fr-fieldset__element">
             <div class="fr-checkbox-group">
-                <input type="checkbox" name="appartient_groupe" class="fr-input" id="id_appartient_groupe" bind:checked={appartientGroupe}>
-                <label class="fr-label" for="id_appartient_groupe">L'entreprise appartient à un groupe composé d'une société-mère et d'une ou plusieurs filiales
+                <input type="checkbox" name="appartient_groupe" class="fr-input" id="{appartientGroupeFieldId}" bind:checked={appartientGroupe}>
+                <label class="fr-label" for="{appartientGroupeFieldId}">L'entreprise appartient à un groupe composé d'une société-mère et d'une ou plusieurs filiales
                     <span class="fr-hint-text"></span>
                 </label>
             </div>
         </div>
 
         {#if appartientGroupe}
-
             <div class="fr-fieldset__element">
                 <div class="fr-select-group">
-                    <label class="fr-label" for="id_effectif_groupe">Effectif du groupe
+                    <label class="fr-label" for="{effectifGroupeFieldId}">Effectif du groupe
                         <span class="fr-hint-text">Nombre de salariés employés par les entreprises du groupe</span>
                     </label>
                     <div class="fr-col-12 ">
-                        <select name="effectif_groupe" class="fr-select" id="id_effectif_groupe"
+                        <select name="effectif_groupe" class="fr-select" id="{effectifGroupeFieldId}"
                         bind:value={effectifGroupe}
                         >
                         <option value="">---------</option>
@@ -174,11 +182,12 @@
                         </select>
                     </div>
                 </div>
+            </div>
 
             <div class="fr-fieldset__element">
                 <div class="fr-checkbox-group">
-                    <input type="checkbox" name="comptes_consolides" class="fr-input" id="id_comptes_consolides" bind:checked={comptesConsolides}>
-                    <label class="fr-label" for="id_comptes_consolides">Le groupe d'entreprises établit des comptes consolidés
+                    <input type="checkbox" name="comptes_consolides" class="fr-input" id="{comptesConsolidesFieldId}" bind:checked={comptesConsolides}>
+                    <label class="fr-label" for="{comptesConsolidesFieldId}">Le groupe d'entreprises établit des comptes consolidés
                         <span class="fr-hint-text"></span>
                     </label>
                 </div>
@@ -187,11 +196,11 @@
             {#if comptesConsolides}
                 <div class="fr-fieldset__element">
                     <div class="fr-select-group">
-                        <label class="fr-label" for="id_tranche_chiffre_affaires_consolide">Chiffre d'affaires consolidé du groupe
+                        <label class="fr-label" for="{trancheChiffreAffairesConsolideFieldId}">Chiffre d'affaires consolidé du groupe
                             <span class="fr-hint-text"></span>
                         </label>
                         <div class="fr-col-12 ">
-                            <select name="tranche_chiffre_affaires_consolide" class="fr-select" id="id_tranche_chiffre_affaires_consolide"
+                            <select name="tranche_chiffre_affaires_consolide" class="fr-select" id="{trancheChiffreAffairesConsolideFieldId}"
                                 bind:value={trancheChiffreAffairesConsolide}
                             >
                                 <option value="">---------</option>
@@ -208,11 +217,11 @@
 
                 <div class="fr-fieldset__element">
                     <div class="fr-select-group">
-                        <label class="fr-label" for="id_tranche_bilan_consolide">Bilan consolidé du groupe
+                        <label class="fr-label" for="{trancheBilanConsolideFieldId}">Bilan consolidé du groupe
                             <span class="fr-hint-text"></span>
                         </label>
                         <div class="fr-col-12 ">
-                            <select name="tranche_bilan_consolide" class="fr-select" id="id_tranche_bilan_consolide"
+                            <select name="tranche_bilan_consolide" class="fr-select" id="{trancheBilanConsolideFieldId}"
                                 bind:value={trancheBilanConsolide}
                             >
                                 <option value="">---------</option>
@@ -227,7 +236,6 @@
                     </div>
                 </div>
             {/if}
-        </div>
         {/if}
     </fieldset>
     <input type="submit" value="Vérifier mes obligations" class="fr-btn">
