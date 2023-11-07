1d7e370379c9785541daf0830e9f449272698239
Brunelie Lauret <brunelie@yaal.coop>
2023-11-07 09:42:42 +0100

    ajout des liens des nouvelles fiches

--- impact/reglementations/views/audit_energetique.py
+++ impact/reglementations/views/audit_energetique.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.urls import reverse_lazy
 
 from entreprises.models import CaracteristiquesAnnuelles
 from reglementations.views.base import Reglementation
@@ -9,9 +10,7 @@ from reglementations.views.base import ReglementationStatus
 class AuditEnergetiqueReglementation(Reglementation):
     title = "Audit énergétique"
     description = "Le code de l'énergie prévoit la réalisation d’un audit énergétique pour les grandes entreprises de plus de 250 salariés, afin qu’elles mettent en place une stratégie d’efficacité énergétique de leurs activités. L’audit énergétique permet de repérer les gisements d’économies d’énergie chez les plus gros consommateurs professionnels (tertiaires et industriels). L’audit doit dater de moins de 4 ans."
-    more_info_url = (
-        "https://www.ecologie.gouv.fr/audit-energetique-des-grandes-entreprises"
-    )
+    more_info_url = reverse_lazy("reglementations:fiche_audit_energetique")
 
     @staticmethod
     def criteres_remplis(caracteristiques):
