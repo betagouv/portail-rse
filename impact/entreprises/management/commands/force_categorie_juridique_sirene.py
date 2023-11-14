7effd8e461a58a66baec698857676de986f9bcf6
Stéphane Blondon <stephane.blondon@gmail.com>
2023-11-14 14:30:58 +0100

    fix: vérification du remplissage de la catégorie juridique d'une entreprise, gestion de l'attente de l'API recherche-entreprise

--- impact/entreprises/management/commands/force_categorie_juridique_sirene.py
+++ impact/entreprises/management/commands/force_categorie_juridique_sirene.py
@@ -1,3 +1,5 @@
+import time
+
 from django.core.management.base import BaseCommand
 
 import api.exceptions
@@ -8,17 +10,23 @@ from entreprises.models import Entreprise
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for entreprise in Entreprise.objects.all():
-            if entreprise.denomination:
+            if entreprise.categorie_juridique_sirene:
                 print(f"IGNORE: {entreprise.siren} {entreprise.denomination}")
             else:
                 try:
-                    infos_entreprise = api.recherche_entreprises.recherche(
-                        entreprise.siren
-                    )
-                    entreprise.categorie_juridique_sirene = infos_entreprise[
-                        "categorie_juridique_sirene"
-                    ]
-                    entreprise.save()
+                    maj(entreprise)
                     print(f"OK: {entreprise.siren} {entreprise.denomination}")
+                except api.exceptions.TooManyRequestError as e:
+                    time.sleep(1)
+                    maj(entreprise)
+                    print(f"OK (2): {entreprise.siren} {entreprise.denomination}")
                 except api.exceptions.APIError as e:
                     print(f"ERREUR {e}: {entreprise.siren}")
+
+
+def maj(entreprise):
+    infos_entreprise = api.recherche_entreprises.recherche(entreprise.siren)
+    entreprise.categorie_juridique_sirene = infos_entreprise[
+        "categorie_juridique_sirene"
+    ]
+    entreprise.save()
