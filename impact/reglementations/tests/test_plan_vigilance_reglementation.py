d8723473a5b3c13b4ceaa1692f411508d72d0b03
Stéphane Blondon <stephane.blondon@gmail.com>
2023-12-08 19:06:34 +0100

    catégorie juridique obligatoire pour que le plan de vigilance soit déterminable

--- impact/reglementations/tests/test_plan_vigilance_reglementation.py
+++ impact/reglementations/tests/test_plan_vigilance_reglementation.py
@@ -5,6 +5,8 @@ from habilitations.models import attach_user_to_entreprise
 from reglementations.views.base import ReglementationStatus
 from reglementations.views.plan_vigilance import PlanVigilanceReglementation
 
+CODE_SA = 5505
+
 
 def test_reglementation_info():
     info = PlanVigilanceReglementation.info()
@@ -18,6 +20,53 @@ def test_reglementation_info():
     )
     assert not info["more_info_url"]
     assert info["tag"] == "tag-social"
+
+
+@pytest.fixture
+def _caracteristiques_suffisamment_qualifiantes_sans_groupe(entreprise_factory):
+    entreprise = entreprise_factory(
+        siren="000000001",
+        categorie_juridique_sirene=CODE_SA,
+        effectif=CaracteristiquesAnnuelles.EFFECTIF_ENTRE_250_ET_299,
+        appartient_groupe=False,
+    )
+    return entreprise.dernieres_caracteristiques
+
+
+@pytest.fixture
+def _caracteristiques_suffisamment_qualifiantes_avec_groupe(
+    entreprise_factory,
+):
+    entreprise = entreprise_factory(
+        siren="000000002",
+        categorie_juridique_sirene=CODE_SA,
+        effectif=CaracteristiquesAnnuelles.EFFECTIF_ENTRE_250_ET_299,
+        appartient_groupe=True,
+        effectif_groupe=CaracteristiquesAnnuelles.EFFECTIF_ENTRE_250_ET_299,
+    )
+    return entreprise.dernieres_caracteristiques
+
+
+def test_est_suffisamment_qualifiee(
+    _caracteristiques_suffisamment_qualifiantes_sans_groupe,
+    _caracteristiques_suffisamment_qualifiantes_avec_groupe,
+):
+    caracteristiques = _caracteristiques_suffisamment_qualifiantes_sans_groupe
+
+    assert PlanVigilanceReglementation.est_suffisamment_qualifiee(caracteristiques)
+
+    caracteristiques = _caracteristiques_suffisamment_qualifiantes_avec_groupe
+
+    assert PlanVigilanceReglementation.est_suffisamment_qualifiee(caracteristiques)
+
+
+def test_n_est_pas_suffisamment_qualifiee_car_categorie_juridique_sirene_non_renseigne(
+    _caracteristiques_suffisamment_qualifiantes_sans_groupe,
+):
+    caracteristiques = _caracteristiques_suffisamment_qualifiantes_sans_groupe
+    caracteristiques.entreprise.categorie_juridique_sirene = None
+
+    assert not PlanVigilanceReglementation.est_suffisamment_qualifiee(caracteristiques)
 
 
 @pytest.mark.parametrize(
@@ -44,6 +93,7 @@ def test_calcule_statut_moins_de_5000_employes(
     effectif, effectif_groupe, entreprise_factory, alice
 ):
     entreprise = entreprise_factory(
+        categorie_juridique_sirene=CODE_SA,
         effectif=effectif,
         appartient_groupe=True if effectif_groupe else False,
         effectif_groupe=effectif_groupe,
@@ -71,7 +121,10 @@ def test_calcule_statut_moins_de_5000_employes(
 def test_calcule_statut_plus_de_5000_employes_dans_l_entreprise(
     effectif, entreprise_factory, alice
 ):
-    entreprise = entreprise_factory(effectif=effectif)
+    entreprise = entreprise_factory(
+        effectif=effectif,
+        categorie_juridique_sirene=CODE_SA,
+    )
     attach_user_to_entreprise(alice, entreprise, "Présidente")
 
     reglementation = PlanVigilanceReglementation.calculate_status(
@@ -99,6 +152,7 @@ def test_calcule_statut_plus_de_5000_employes_dans_le_groupe(
     effectif_groupe, entreprise_factory, alice
 ):
     entreprise = entreprise_factory(
+        categorie_juridique_sirene=CODE_SA,
         effectif=CaracteristiquesAnnuelles.EFFECTIF_MOINS_DE_50,
         appartient_groupe=True,
         effectif_groupe=effectif_groupe,
