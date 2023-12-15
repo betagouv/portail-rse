fc2450cac1e62b881776911604326eebaff68873
Stéphane Blondon <stephane.blondon@gmail.com>
2023-12-15 18:09:04 +0100

    attrape toute exception qui pourrait arriver si l'API changeait

--- impact/api/bges.py
+++ impact/api/bges.py
@@ -11,11 +11,14 @@ def bges_publication_year(siren):
         sentry_sdk.capture_message(f"Erreur API bilans-ges ({siren})")
         return
 
-    data = response.json()
-    if members := data["hydra:member"]:
-        first_member = members[0]
-        last_year = first_member["identitySheet"]["reportingYear"]
-        for member in members[1:]:
-            year = member["identitySheet"]["reportingYear"]
-            last_year = max(last_year, year)
-        return last_year
+    try:
+        data = response.json()
+        if members := data["hydra:member"]:
+            first_member = members[0]
+            last_year = first_member["identitySheet"]["reportingYear"]
+            for member in members[1:]:
+                year = member["identitySheet"]["reportingYear"]
+                last_year = max(last_year, year)
+            return last_year
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
