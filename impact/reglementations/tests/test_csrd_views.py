ac8b416f174ca51016bfecf5a3c50115099b3c52
emillumine <camille@yaal.coop>
2023-12-20 12:25:28 +0100

    test: ajoute un test sur la nouvelle page csrd

---
+++ impact/reglementations/tests/test_csrd_views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+
+
+def test_bdese_is_not_public(client, alice):
+    url = f"/csrd"
+    response = client.get(url)
+
+    assert response.status_code == 302
+    connexion_url = reverse("users:login")
+    assert response.url == f"{connexion_url}?next={url}"
+
+    client.force_login(alice)
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.templates[0].name == "reglementations/espace-csrd.html"
