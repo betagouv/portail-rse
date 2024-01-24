8892cb2be27fa2d3e39915db594da26282df062c
emillumine <camille@yaal.coop>
2024-01-24 15:46:16 +0100

    simplifie le groupement des réglementations par statut dans le contexte pour faciliter l'intégration dans le template et la lecture du code

--- impact/reglementations/views/__init__.py
+++ impact/reglementations/views/__init__.py
@@ -47,14 +47,42 @@ def tableau_de_bord(request, siren):
                 request,
                 f"Les réglementations affichées sont basées sur des informations de l'exercice comptable {caracteristiques.annee}. <a href='{reverse_lazy('entreprises:qualification', args=[entreprise.siren])}'>Mettre à jour les informations de l'entreprise.</a>",
             )
+        reglementations = calcule_reglementations(caracteristiques, request.user)
+        reglementations_a_actualiser = [
+            r
+            for r in reglementations
+            if r["status"].status == ReglementationStatus.STATUS_A_ACTUALISER
+        ]
+        reglementations_en_cours = [
+            r
+            for r in reglementations
+            if r["status"].status == ReglementationStatus.STATUS_EN_COURS
+        ]
+        reglementations_a_jour = [
+            r
+            for r in reglementations
+            if r["status"].status == ReglementationStatus.STATUS_A_JOUR
+        ]
+        reglementations_soumises = [
+            r
+            for r in reglementations
+            if r["status"].status == ReglementationStatus.STATUS_SOUMIS
+        ]
+        reglementations_non_soumises = [
+            r
+            for r in reglementations
+            if r["status"].status == ReglementationStatus.STATUS_NON_SOUMIS
+        ]
         return render(
             request,
             "reglementations/tableau_de_bord.html",
             context={
                 "entreprise": entreprise,
-                "reglementations": trie_reglementations_par_status(
-                    caracteristiques, request.user
-                ),
+                "reglementations_a_actualiser": reglementations_a_actualiser,
+                "reglementations_en_cours": reglementations_en_cours,
+                "reglementations_a_jour": reglementations_a_jour,
+                "reglementations_soumises": reglementations_soumises,
+                "reglementations_non_soumises": reglementations_non_soumises,
             },
         )
     else:
@@ -63,15 +91,6 @@ def tableau_de_bord(request, siren):
             "Veuillez renseigner les informations suivantes pour accéder au tableau de bord de cette entreprise.",
         )
         return redirect("entreprises:qualification", siren=entreprise.siren)
-
-
-def trie_reglementations_par_status(
-    caracteristiques: CaracteristiquesAnnuelles, user: settings.AUTH_USER_MODEL
-):
-    tri = {status.label: [] for status in ReglementationStatus.status_possibles()}
-    for reglementation_et_status in calcule_reglementations(caracteristiques, user):
-        tri[reglementation_et_status["status"].label].append(reglementation_et_status)
-    return tri
 
 
 def calcule_reglementations(
