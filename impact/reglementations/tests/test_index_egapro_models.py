b90c520092f62aabc6e018158c6017bb2cf91bae
emillumine <camille@yaal.coop>
2024-02-01 15:24:47 +0100

    refacto: renomme une fonction

--- impact/reglementations/tests/test_index_egapro_models.py
+++ impact/reglementations/tests/test_index_egapro_models.py
@@ -2,17 +2,17 @@ from datetime import date
 
 from freezegun import freeze_time
 
-from reglementations.models import derniere_annee_a_remplir_index_egapro
+from reglementations.models import derniere_annee_a_publier_index_egapro
 from reglementations.models import prochaine_echeance_index_egapro
 
 
-def test_derniere_annee_a_remplir_index_egapro():
+def test_derniere_annee_a_publier_index_egapro():
     with freeze_time("2022-11-23"):
-        annee = derniere_annee_a_remplir_index_egapro()
+        annee = derniere_annee_a_publier_index_egapro()
         assert annee == 2021
 
     with freeze_time("2023-11-23"):
-        annee = derniere_annee_a_remplir_index_egapro()
+        annee = derniere_annee_a_publier_index_egapro()
         assert annee == 2022
 
 
