7ba7afbcf7d4c64e3f2895b76fe1f6269c0e28ff
emillumine <camille@yaal.coop>
2024-02-16 18:06:17 +0100

    soumet les grandes entreprises non cotées à la CSRD à partir de 2026

--- impact/reglementations/views/csrd.py
+++ impact/reglementations/views/csrd.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
+from entreprises.models import CaracteristiquesAnnuelles
 from reglementations.views.base import Reglementation
 
 
@@ -10,6 +11,26 @@ class CSRDReglementation(Reglementation):
     more_info_url = reverse_lazy("reglementations:fiche_csrd")
     tag = "tag-durabilite"
     summary = "Publier un rapport de durabilité"
+
+    @classmethod
+    def est_suffisamment_qualifiee(cls, caracteristiques):
+        return (
+            caracteristiques.entreprise.est_cotee is not None
+            and caracteristiques.effectif is not None
+            and caracteristiques.tranche_bilan is not None
+            and caracteristiques.tranche_chiffre_affaires is not None
+            and caracteristiques.entreprise.appartient_groupe is not None
+        )
+
+    @classmethod
+    def est_soumis(cls, caracteristiques):
+        return bool(cls.est_soumis_a_partir_de(caracteristiques))
+
+    @classmethod
+    def est_soumis_a_partir_de(
+        cls, caracteristiques: CaracteristiquesAnnuelles
+    ) -> int | None:
+        return 2026
 
 
 @login_required
