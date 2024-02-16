5cb4e31a856726ee1d51cccb0679e43f8282784d
Stéphane <stephane@yaal.coop>
2024-02-16 19:23:53 +0100

    ajout de cas soumis et non soumis à la CSRD

--- impact/reglementations/views/csrd.py
+++ impact/reglementations/views/csrd.py
@@ -30,7 +30,33 @@ class CSRDReglementation(Reglementation):
     def est_soumis_a_partir_de(
         cls, caracteristiques: CaracteristiquesAnnuelles
     ) -> int | None:
+        if cls.est_microentreprise(caracteristiques):
+            return None
+        if caracteristiques.entreprise.est_cotee:
+            if caracteristiques.effectif in (
+                CaracteristiquesAnnuelles.EFFECTIF_ENTRE_500_ET_4999,
+                CaracteristiquesAnnuelles.EFFECTIF_ENTRE_5000_ET_9999,
+                CaracteristiquesAnnuelles.EFFECTIF_10000_ET_PLUS,
+            ):
+                return 2025
         return 2026
+
+    @classmethod
+    def est_microentreprise(cls, caracteristiques: CaracteristiquesAnnuelles):
+        score = 0
+        if (
+            caracteristiques.tranche_bilan
+            == CaracteristiquesAnnuelles.BILAN_MOINS_DE_350K
+        ):
+            score += 1
+        if (
+            caracteristiques.tranche_chiffre_affaires
+            == CaracteristiquesAnnuelles.CA_MOINS_DE_700K
+        ):
+            score += 1
+        if caracteristiques.effectif == CaracteristiquesAnnuelles.EFFECTIF_MOINS_DE_50:
+            score += 1
+        return score >= 2
 
 
 @login_required
