50fc5f3656470bcb20eb301eb6645a5f4e12326b
emillumine <camille@yaal.coop>
2024-02-23 18:08:35 +0100

    supprime le doublon sur la page des réglementations RSE

--- impact/reglementations/tests/test_csrd_reglementation.py
+++ impact/reglementations/tests/test_csrd_reglementation.py
@@ -10,7 +10,7 @@ from reglementations.views.csrd import CSRDReglementation
 def test_reglementation_info():
     info = CSRDReglementation.info()
 
-    assert info["title"] == "Rapport de durabilité - Directive CSRD"
+    assert info["title"] == "Rapport de Durabilité - CSRD"
     assert info["more_info_url"] == reverse("reglementations:fiche_csrd")
     assert info["tag"] == "tag-durabilite"
     assert info["summary"] == "Publier un rapport de durabilité"
