c0a5d2e2ef4a46c2838934a4595c583f542d2301
emillumine <camille@yaal.coop>
2024-03-15 12:21:14 +0100

    augmente le nombre de workers de gunicorn pour tenter d'améliorer le nombre de requêtes traitées en parallèle par défaut il était à 1

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 python impact/manage.py collectstatic --no-input
 
-gunicorn --pythonpath=impact impact.wsgi --log-file -
+# On suppose qu'il y a au moins 8 cores sur la machine et d'après la doc de gunicorn le nombre de workers conseillé est (2 x $num_cores) + 1
+# https://docs.gunicorn.org/en/stable/design.html#how-many-workers
+gunicorn --workers=17 --pythonpath=impact impact.wsgi --log-file -
