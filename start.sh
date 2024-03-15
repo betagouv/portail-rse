70153ccc0a3d46b7d0cd763a71c1f95cc68e6aed
emillumine <camille@yaal.coop>
2024-03-15 12:37:33 +0100

    diminue le nombre de workers gunicorn car faisait exploser la mémoire

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 python impact/manage.py collectstatic --no-input
 
-# On suppose qu'il y a au moins 8 cores sur la machine et d'après la doc de gunicorn le nombre de workers conseillé est (2 x $num_cores) + 1
+# En supposant qu'il y avait au moins 8 cores sur la machine et d'après la doc de gunicorn le nombre de workers conseillé est (2 x $num_cores) + 1
 # https://docs.gunicorn.org/en/stable/design.html#how-many-workers
-gunicorn --workers=17 --pythonpath=impact impact.wsgi --log-file -
+# Ce n'était pas concluant (explosion de la mémoire)
+gunicorn --workers=3 --pythonpath=impact impact.wsgi --log-file -
