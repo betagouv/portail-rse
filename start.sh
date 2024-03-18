5228ada533721363adddb53d9f0b3b41c6f4abe4
emillumine <camille@yaal.coop>
2024-03-18 18:20:49 +0100

    tente de résoudre l'augmentation de la mémoire consommée sur la prod observée sur scalingo depuis quelques jours réinitialise les options de lancement de gunicorn telles qu'écrites dans la doc de scalingo (sur le déploiement d'une app django)

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 python impact/manage.py collectstatic --no-input
 
-# En supposant qu'il y avait au moins 8 cores sur la machine et d'après la doc de gunicorn le nombre de workers conseillé est (2 x $num_cores) + 1
+# En supposant qu'il y avait au moins 8 cores sur la machine et d'après la doc de gunicorn le nombre de workers conseillé était de (2 x $num_cores) + 1
 # https://docs.gunicorn.org/en/stable/design.html#how-many-workers
-# Ce n'était pas concluant (explosion de la mémoire)
-gunicorn --workers=3 --pythonpath=impact impact.wsgi --log-file -
+# Ce n'était pas concluant (explosion de la mémoire avec l'option --workers=17)
+gunicorn --pythonpath=impact impact.wsgi --log-file -
