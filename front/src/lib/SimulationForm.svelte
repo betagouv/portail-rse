c73e98338fbf830ceb471727d73304f41e612575
Stéphane <stephane@yaal.coop>
2024-03-29 19:11:46 +0100

    refacto: les spécificités du formulaire du simulation sont séparées du composant de recherche d'entreprise

---
+++ front/src/lib/SimulationForm.svelte
@@ -0,0 +1,101 @@
+<script>
+    import SearchEntreprise from './SearchEntreprise.svelte'
+
+    export let siren = ""
+    export let denomination = ""
+
+    // defined by django
+    const sirenFieldId = "id_siren"
+    const sirenFieldName = "siren"
+    const denominationFieldName = "denomination"
+    const categorieJuridiqueFieldId = "id_categorie_juridique_sirene"
+    const codePaysEtrangerFieldId = "id_code_pays_etranger_sirene"
+    const effectifFieldId = "id_effectif"
+    const trancheChiffreAffairesFieldId = "id_tranche_chiffre_affaires"
+    const appartientGroupeFieldId = "id_appartient_groupe"
+    const comptesConsolidesFieldId = "id_comptes_consolides"
+    const trancheChiffreAffairesConsolideFieldId = "id_tranche_chiffre_affaires_consolide"
+
+    const categorieJuridiqueField = document.getElementById(categorieJuridiqueFieldId)
+    const codePaysEtrangerField = document.getElementById(codePaysEtrangerFieldId)
+    const effectifField = document.getElementById(effectifFieldId)
+    const trancheChiffreAffairesField = document.getElementById(trancheChiffreAffairesFieldId)
+    const appartientGroupeField = document.getElementById(appartientGroupeFieldId)
+    const comptesConsolidesField = document.getElementById(comptesConsolidesFieldId)
+    const trancheChiffreAffairesConsolideField = document.getElementById(trancheChiffreAffairesConsolideFieldId)
+    const submitButton = document.getElementById(sirenFieldId).closest("form").querySelector("[type=submit]")
+
+    const simulationFields = document.getElementById("svelte-simulation-fields")
+
+    const showSimulationFields = () => {
+        if (simulationFields) {
+            simulationFields.style.display = "block"
+        }
+    }
+
+    const hideSimulationFields = () => {
+        if (simulationFields) {
+            simulationFields.style.display = "none"
+        }
+    }
+
+    const resetSimulationFields = () => {
+        if (simulationFields) {
+            for(const checkboxField of simulationFields.querySelectorAll('[type="checkbox"]')) {
+                checkboxField.checked = false
+            }
+            for(const selectField of simulationFields.getElementsByTagName("select")) {
+                selectField.value = ""
+            }
+            const event = new Event("change");
+            appartientGroupeField.dispatchEvent(event);
+            comptesConsolidesField.dispatchEvent(event);
+        }
+    }
+
+    const updateSimulationFields = (infos) => {
+        console.log(infos)
+        resetSimulationFields()
+        denomination = infos.denomination
+        effectifField.value = infos.effectif
+        categorieJuridiqueField.value = infos.categorie_juridique_sirene
+        codePaysEtrangerField.value = infos.code_pays_etranger_sirene
+        if (infos.tranche_chiffre_affaires) {
+            // si l'API ne renvoie pas de chiffre
+            // d'affaires, ne pas préremplir le champ
+            // pour garder le label par défaut
+            trancheChiffreAffairesField.value = infos.tranche_chiffre_affaires
+        }
+        if (infos.tranche_chiffre_affaires_consolide) { // si chiffre d'affaires consolidé, alors est un groupe avec compte consolidé
+            appartientGroupeField.checked = true
+            comptesConsolidesField.checked = true
+            trancheChiffreAffairesConsolideField.value = infos.tranche_chiffre_affaires_consolide
+            const event = new Event("change");
+            appartientGroupeField.dispatchEvent(event);
+            comptesConsolidesField.dispatchEvent(event);
+        }
+    }
+
+    document.addEventListener(
+        "infos-entreprise",
+        (event) => {
+            showSimulationFields()
+            updateSimulationFields(event.detail)
+        }
+    )
+
+    document.addEventListener(
+        "siren-incorrect",
+        () => {hideSimulationFields()}
+    )
+
+    if (siren) {
+        showSimulationFields()
+    }
+    else {
+        submitButton.disabled = true
+        hideSimulationFields()
+    }
+</script>
+
+<SearchEntreprise siren={siren} denomination={denomination} />
