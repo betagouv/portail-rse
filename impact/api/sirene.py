e359720f7b52b6b5cc695369d28c8dfb5b2aa29e
Stéphane <stephane@yaal.coop>
2024-04-16 19:27:37 +0200

    ajout de cas d'erreurs de l'API Sirene

--- impact/api/sirene.py
+++ impact/api/sirene.py
@@ -1,45 +1,74 @@
 from datetime import date
 
 import requests
+import sentry_sdk
 
+from .exceptions import APIError
+from .exceptions import ServerError
+from .exceptions import SirenError
+from .exceptions import TooManyRequestError
 from entreprises.models import CaracteristiquesAnnuelles
 from impact.settings import API_SIRENE_TOKEN
+
+RECHERCHE_ENTREPRISE_TIMEOUT = 10
+SIREN_NOT_FOUND_ERROR = (
+    "L'entreprise n'a pas été trouvée. Vérifiez que le SIREN est correct."
+)
+TOO_MANY_REQUESTS_ERROR = "Le service est temporairement surchargé. Merci de réessayer."
+SERVER_ERROR = "Le service est actuellement indisponible. Merci de réessayer plus tard."
 
 
 def recherche_unite_legale(siren):
     # documentation api sirene 3.11 https://www.sirene.fr/static-resources/htm/sommaire_311.html
     url = f"https://api.insee.fr/entreprises/sirene/V3.11/siren/{siren}?date={date.today().isoformat()}"
-    response = requests.get(
-        url, headers={"Authorization": f"Bearer {API_SIRENE_TOKEN}"}
-    )
+    try:
+        response = requests.get(
+            url,
+            headers={"Authorization": f"Bearer {API_SIRENE_TOKEN}"},
+            timeout=RECHERCHE_ENTREPRISE_TIMEOUT,
+        )
+    except Exception as e:
+        with sentry_sdk.push_scope() as scope:
+            scope.set_level("info")
+            sentry_sdk.capture_exception(e)
+        raise APIError(SERVER_ERROR)
 
-    data = response.json()["uniteLegale"]
+    if response.status_code == 200:
+        data = response.json()["uniteLegale"]
 
-    denomination = data["periodesUniteLegale"][0]["denominationUniteLegale"]
-    categorie_juridique_sirene = int(
-        data["periodesUniteLegale"][0]["categorieJuridiqueUniteLegale"]
-    )
-    tranche_effectif = int(data["trancheEffectifsUniteLegale"])
-    if tranche_effectif < 11:  # moins de 10 salariés
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_MOINS_DE_10
-    elif tranche_effectif < 21:  # moins de 50 salariés
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_10_ET_49
-    elif tranche_effectif < 32:  # moins de 250 salariés
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_50_ET_249
-    # la tranche EFFECTIF_ENTRE_250_ET_299 ne peut pas être trouvée avec l'API
-    elif tranche_effectif < 41:  # moins de 500 salariés
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_300_ET_499
-    elif tranche_effectif < 52:  # moins de 5 000 salariés:
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_500_ET_4999
-    elif tranche_effectif == 52:
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_5000_ET_9999
+        denomination = data["periodesUniteLegale"][0]["denominationUniteLegale"]
+        categorie_juridique_sirene = int(
+            data["periodesUniteLegale"][0]["categorieJuridiqueUniteLegale"]
+        )
+        tranche_effectif = int(data["trancheEffectifsUniteLegale"])
+        if tranche_effectif < 11:  # moins de 10 salariés
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_MOINS_DE_10
+        elif tranche_effectif < 21:  # moins de 50 salariés
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_10_ET_49
+        elif tranche_effectif < 32:  # moins de 250 salariés
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_50_ET_249
+        # la tranche EFFECTIF_ENTRE_250_ET_299 ne peut pas être trouvée avec l'API
+        elif tranche_effectif < 41:  # moins de 500 salariés
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_300_ET_499
+        elif tranche_effectif < 52:  # moins de 5 000 salariés:
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_500_ET_4999
+        elif tranche_effectif == 52:
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_ENTRE_5000_ET_9999
+        else:
+            effectif = CaracteristiquesAnnuelles.EFFECTIF_10000_ET_PLUS
+
+        return {
+            "siren": siren,
+            "effectif": effectif,
+            "denomination": denomination,
+            "categorie_juridique_sirene": categorie_juridique_sirene,
+            "code_pays_etranger_sirene": None,
+        }
+    elif response.status_code == 404:
+        raise SirenError(SIREN_NOT_FOUND_ERROR)
+    elif response.status_code == 429:
+        sentry_sdk.capture_message("Trop de requêtes sur l'API recherche unité légale")
+        raise TooManyRequestError(TOO_MANY_REQUESTS_ERROR)
     else:
-        effectif = CaracteristiquesAnnuelles.EFFECTIF_10000_ET_PLUS
-
-    return {
-        "siren": siren,
-        "effectif": effectif,
-        "denomination": denomination,
-        "categorie_juridique_sirene": categorie_juridique_sirene,
-        "code_pays_etranger_sirene": None,
-    }
+        sentry_sdk.capture_message("Erreur API recherche unité légale")
+        raise ServerError(SERVER_ERROR)
--- impact/api/tests/test_sirene.py
+++ impact/api/tests/test_sirene.py
@@ -1,5 +1,14 @@
-import pytest
+from datetime import date
+from unittest import mock
 
+import pytest
+from requests.exceptions import Timeout
+
+from api.exceptions import APIError
+from api.exceptions import ServerError
+from api.exceptions import SirenError
+from api.exceptions import TooManyRequestError
+from api.sirene import RECHERCHE_ENTREPRISE_TIMEOUT
 from api.sirene import recherche_unite_legale
 from entreprises.models import CaracteristiquesAnnuelles
 
@@ -16,3 +25,83 @@ def test_api_fonctionnelle():
         "categorie_juridique_sirene": 7120,
         "code_pays_etranger_sirene": None,
     }
+
+
+def test_succès_pas_de_résultat(mocker):
+    SIREN = "000000000"
+
+    class FakeResponse:
+        status_code = 404
+
+    faked_request = mocker.patch("requests.get", return_value=FakeResponse())
+
+    with pytest.raises(SirenError) as e:
+        recherche_unite_legale(SIREN)
+
+    faked_request.assert_called_once_with(
+        f"https://api.insee.fr/entreprises/sirene/V3.11/siren/000000000?date={date.today().isoformat()}",
+        headers={"Authorization": mock.ANY},
+        timeout=RECHERCHE_ENTREPRISE_TIMEOUT,
+    )
+    assert (
+        str(e.value)
+        == "L'entreprise n'a pas été trouvée. Vérifiez que le SIREN est correct."
+    )
+
+
+def test_echec_exception_provoquee_par_l_api(mocker):
+    """le Timeout est un cas réel mais l'implémentation attrape toutes les erreurs possibles"""
+    SIREN = "123456789"
+
+    faked_request = mocker.patch("requests.get", side_effect=Timeout)
+    capture_exception_mock = mocker.patch("sentry_sdk.capture_exception")
+
+    with pytest.raises(APIError) as e:
+        recherche_unite_legale(SIREN)
+
+    capture_exception_mock.assert_called_once()
+    args, _ = capture_exception_mock.call_args
+    assert type(args[0]) == Timeout
+    assert (
+        str(e.value)
+        == "Le service est actuellement indisponible. Merci de réessayer plus tard."
+    )
+
+
+def test_echec_trop_de_requetes(mocker):
+    SIREN = "123456789"
+
+    class FakeResponse:
+        status_code = 429
+
+    mocker.patch("requests.get", return_value=FakeResponse())
+    capture_message_mock = mocker.patch("sentry_sdk.capture_message")
+
+    with pytest.raises(TooManyRequestError) as e:
+        recherche_unite_legale(SIREN)
+
+    capture_message_mock.assert_called_once_with(
+        "Trop de requêtes sur l'API recherche unité légale"
+    )
+    assert (
+        str(e.value) == "Le service est temporairement surchargé. Merci de réessayer."
+    )
+
+
+def test_echec_erreur_de_l_API(mocker):
+    SIREN = "123456789"
+
+    class FakeResponse:
+        status_code = 500
+
+    mocker.patch("requests.get", return_value=FakeResponse())
+    capture_message_mock = mocker.patch("sentry_sdk.capture_message")
+
+    with pytest.raises(ServerError) as e:
+        recherche_unite_legale(SIREN)
+
+    capture_message_mock.assert_called_once_with("Erreur API recherche unité légale")
+    assert (
+        str(e.value)
+        == "Le service est actuellement indisponible. Merci de réessayer plus tard."
+    )
