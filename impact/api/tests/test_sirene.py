ede0f64f695529dddcb860e80549510a14aa3755
emillumine <camille@yaal.coop>
2024-04-17 11:16:25 +0200

    refacto: simplifie des tests

--- impact/api/tests/test_sirene.py
+++ impact/api/tests/test_sirene.py
@@ -13,6 +13,15 @@ from api.sirene import SIRENE_TIMEOUT
 from entreprises.models import CaracteristiquesAnnuelles
 
 
+class MockedResponse:
+    def __init__(self, status_code, json_content=""):
+        self.status_code = status_code
+        self.json_content = json_content
+
+    def json(self):
+        return self.json_content
+
+
 @pytest.mark.network
 def test_api_fonctionnelle():
     SIREN = "130025265"
@@ -29,27 +38,23 @@ def test_api_fonctionnelle():
 
 def test_succès_avec_résultat(mocker):
     SIREN = "123456789"
-
-    class FakeResponse:
-        status_code = 200
-
-        def json(self):
-            # la plupart des champs inutilisés de la réponse ont été supprimés
-            return {
-                "header": {"message": "OK", "statut": 200},
-                "uniteLegale": {
-                    "periodesUniteLegale": [
-                        {
-                            "categorieJuridiqueUniteLegale": "5710",
-                            "denominationUniteLegale": "ENTREPRISE",
-                        }
-                    ],
-                    "siren": "123456789",
-                    "trancheEffectifsUniteLegale": "20",
-                },
-            }
-
-    faked_request = mocker.patch("requests.get", return_value=FakeResponse())
+    # la plupart des champs inutilisés de la réponse ont été supprimés
+    json_content = {
+        "header": {"message": "OK", "statut": 200},
+        "uniteLegale": {
+            "periodesUniteLegale": [
+                {
+                    "categorieJuridiqueUniteLegale": "5710",
+                    "denominationUniteLegale": "ENTREPRISE",
+                }
+            ],
+            "siren": "123456789",
+            "trancheEffectifsUniteLegale": "20",
+        },
+    }
+    faked_request = mocker.patch(
+        "requests.get", return_value=MockedResponse(200, json_content)
+    )
 
     infos = recherche_unite_legale(SIREN)
 
@@ -69,20 +74,12 @@ def test_succès_avec_résultat(mocker):
 
 def test_succès_pas_de_résultat(mocker):
     SIREN = "000000000"
-
-    class FakeResponse:
-        status_code = 404
-
-    faked_request = mocker.patch("requests.get", return_value=FakeResponse())
+    # un siren non trouvé renvoie une 404
+    faked_request = mocker.patch("requests.get", return_value=MockedResponse(404))
 
     with pytest.raises(SirenError) as e:
         recherche_unite_legale(SIREN)
 
-    faked_request.assert_called_once_with(
-        f"https://api.insee.fr/entreprises/sirene/V3.11/siren/000000000?date={date.today().isoformat()}",
-        headers={"Authorization": mock.ANY},
-        timeout=SIRENE_TIMEOUT,
-    )
     assert (
         str(e.value)
         == "L'entreprise n'a pas été trouvée. Vérifiez que le SIREN est correct."
@@ -92,8 +89,7 @@ def test_succès_pas_de_résultat(mocker):
 def test_echec_exception_provoquee_par_l_api(mocker):
     """le Timeout est un cas réel mais l'implémentation attrape toutes les erreurs possibles"""
     SIREN = "123456789"
-
-    faked_request = mocker.patch("requests.get", side_effect=Timeout)
+    mocker.patch("requests.get", side_effect=Timeout)
     capture_exception_mock = mocker.patch("sentry_sdk.capture_exception")
 
     with pytest.raises(APIError) as e:
@@ -110,11 +106,7 @@ def test_echec_exception_provoquee_par_l_api(mocker):
 
 def test_echec_trop_de_requetes(mocker):
     SIREN = "123456789"
-
-    class FakeResponse:
-        status_code = 429
-
-    mocker.patch("requests.get", return_value=FakeResponse())
+    mocker.patch("requests.get", return_value=MockedResponse(429))
     capture_message_mock = mocker.patch("sentry_sdk.capture_message")
 
     with pytest.raises(TooManyRequestError) as e:
@@ -128,11 +120,7 @@ def test_echec_trop_de_requetes(mocker):
 
 def test_echec_erreur_de_l_API(mocker):
     SIREN = "123456789"
-
-    class FakeResponse:
-        status_code = 500
-
-    mocker.patch("requests.get", return_value=FakeResponse())
+    mocker.patch("requests.get", return_value=MockedResponse(500))
     capture_message_mock = mocker.patch("sentry_sdk.capture_message")
 
     with pytest.raises(ServerError) as e:
