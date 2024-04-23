ab2817bb08f644d0e17d9efdffffc7fd0de92813
Stéphane <stephane@yaal.coop>
2024-04-23 16:35:18 +0200

    ajout des informations financières de manière facultative dans les données infos_entreprise

--- impact/api/infos_entreprise.py
+++ impact/api/infos_entreprise.py
@@ -1,10 +1,11 @@
+import api.ratios_financiers
 import api.recherche_entreprises
 import api.sirene
 from api.exceptions import APIError
 from api.exceptions import SirenError
 
 
-def infos_entreprise(siren):
+def infos_entreprise(siren, donnees_financieres=False):
     try:
         infos = api.recherche_entreprises.recherche(siren)
     except SirenError:
@@ -12,4 +13,10 @@ def infos_entreprise(siren):
     except APIError:
         # utilise l'API Sirene en fallback
         infos = api.sirene.recherche_unite_legale(siren)
+
+    if donnees_financieres:
+        try:
+            infos.update(api.ratios_financiers.dernier_exercice_comptable(siren))
+        except APIError:
+            infos.update(api.ratios_financiers.dernier_exercice_comptable_vide())
     return infos
