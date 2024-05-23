45cb3b087623a5444f6f4819e8d0ceeaa5234c78
emillumine <camille@yaal.coop>
2024-05-23 17:37:21 +0200

    récupère et enregistre le code NAF de l'entreprise lorsqu'elle est créée à la création ou au rattachement d'un utilisateur

--- impact/entreprises/views.py
+++ impact/entreprises/views.py
@@ -80,6 +80,7 @@ def search_and_create_entreprise(siren):
         denomination=infos_entreprise["denomination"],
         categorie_juridique_sirene=infos_entreprise["categorie_juridique_sirene"],
         code_pays_etranger_sirene=infos_entreprise["code_pays_etranger_sirene"],
+        code_NAF=infos_entreprise["code_NAF"],
     )
 
 
