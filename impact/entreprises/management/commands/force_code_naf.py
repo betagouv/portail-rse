cfa3689da1b6fb8117867873c0dee2197fd2716a
emillumine <camille@yaal.coop>
2024-05-24 14:32:14 +0200

    refait une tentative API après 1s en cas de dépassement du quota autorisé

--- impact/entreprises/management/commands/force_code_naf.py
+++ impact/entreprises/management/commands/force_code_naf.py
@@ -1,3 +1,5 @@
+import time
+
 from django.core.management.base import BaseCommand
 
 import api.exceptions
@@ -14,13 +16,21 @@ class Command(BaseCommand):
                 )
             else:
                 try:
-                    infos_entreprise = api.recherche_entreprises.recherche(
-                        entreprise.siren
-                    )
-                    entreprise.code_NAF = infos_entreprise["code_NAF"]
-                    entreprise.save()
+                    maj(entreprise)
                     print(
                         f"OK: {entreprise.siren} {entreprise.denomination} {entreprise.code_NAF}"
                     )
+                except api.exceptions.TooManyRequestError as e:
+                    time.sleep(1)
+                    maj(entreprise)
+                    print(
+                        f"OK (2): {entreprise.siren} {entreprise.denomination} {entreprise.code_NAF}"
+                    )
                 except api.exceptions.APIError as e:
                     print(f"ERREUR {e}: {entreprise.siren}")
+
+
+def maj(entreprise):
+    infos_entreprise = api.recherche_entreprises.recherche(entreprise.siren)
+    entreprise.code_NAF = infos_entreprise["code_NAF"]
+    entreprise.save()
