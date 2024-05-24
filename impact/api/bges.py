betagouv/impact

ajoute la date de publication du dernier bilan GES dans les données récupérées par API

impact/api/bges.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 import requests
 import sentry_sdk
 
 
-def last_reporting_year(siren):
+def last_reporting(siren):
     response = requests.get(
         "https://bilans-ges.ademe.fr/api/inventories",
         params={"page": "1", "itemsPerPage": "11", "entity.siren": siren},
@@ -15,10 +17,16 @@ def last_reporting_year(siren):
         data = response.json()
         if members := data["hydra:member"]:
             first_member = members[0]
-            last_year = first_member["identitySheet"]["reportingYear"]
+            last_reporting_year = first_member["identitySheet"]["reportingYear"]
+            publicated_at = first_member["publication"]["publicatedAt"]
             for member in members[1:]:
                 year = member["identitySheet"]["reportingYear"]
-                last_year = max(last_year, year)
-            return last_year
+                if year > last_reporting_year:
+                    last_reporting_year = year
+                    publicated_at = member["publication"]["publicatedAt"]
+            return {
+                "year": last_reporting_year,
+                "publication_date": datetime.fromisoformat(publicated_at).date(),
+            }
     except Exception as e:
         sentry_sdk.capture_exception(e)

betagouv/portail-rse

impact/api/bges.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 import sentry_sdk
 
@@ -8,7 +10,7 @@ NOM_API = "bilans-ges"
 BGES_TIMEOUT = 10
 
 
-def last_reporting_year(siren):
+def dernier_bilan_ges(siren):
     try:
         response = requests.get(
             "https://bilans-ges.ademe.fr/api/inventories",
@@ -29,11 +31,17 @@ def last_reporting_year(siren):
         data = response.json()
         if members := data["hydra:member"]:
             first_member = members[0]
-            last_year = first_member["identitySheet"]["reportingYear"]
+            last_reporting_year = first_member["identitySheet"]["reportingYear"]
+            publicated_at = first_member["publication"]["publicatedAt"]
             for member in members[1:]:
                 year = member["identitySheet"]["reportingYear"]
-                last_year = max(last_year, year)
-            return last_year
+                if year > last_reporting_year:
+                    last_reporting_year = year
+                    publicated_at = member["publication"]["publicatedAt"]
+            return {
+                "annee_reporting": last_reporting_year,
+                "date_publication": datetime.fromisoformat(publicated_at).date(),
+            }
     except Exception as e:
         sentry_sdk.capture_exception(e)
         raise APIError()