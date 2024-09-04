37490a8ad8060cd2a6cffc963f51498b581ca1fb
Frédéric Vergez <fred@ikarius.com>
2024-09-04 11:04:17 +0200

    csp: activation de Sentry pour les rapports

--- impact/utils/context_processors.py
+++ impact/utils/context_processors.py
@@ -3,4 +3,8 @@ from django.conf import settings
 
 def custom_settings(request):
     # allows direct access to some relevant settings
-    return {"matomo_enabled": not settings.MATOMO_DISABLED}
+    return {
+        "matomo_enabled": not settings.MATOMO_DISABLED,
+        "sentry_dsn": settings.SENTRY_DSN,
+        "sentry_env": settings.SENTRY_ENV,
+    }
