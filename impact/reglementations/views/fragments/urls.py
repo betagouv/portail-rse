9d3fc0fe23c665d862b0777d422d400c3e26928a
Frédéric Vergez <fred@ikarius.com>
2024-10-08 11:58:31 +0200

    fragment: ajout du rafraichissement du panneau ESG

--- impact/reglementations/views/fragments/urls.py
+++ impact/reglementations/views/fragments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .enjeux import creation_enjeu
+from .enjeux import rafraichissement_esg
 from .enjeux import selection_enjeux
 from .enjeux import suppression_enjeu
 
@@ -19,5 +20,10 @@ urlpatterns = [
         "csrd/fragments/suppression_enjeu/<str:enjeu_id>",
         suppression_enjeu,
         name="suppression_enjeu",
+    ),
+    path(
+        "csrd/fragments/rafraichissement_esg/<str:csrd_id>",
+        rafraichissement_esg,
+        name="rafraichissement_esg",
     ),
 ]
