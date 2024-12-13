505a740378896049b62567cabce97c9ffd5267cd
Frédéric Vergez <fred@ikarius.com>
2024-12-13 20:38:07 +0100

    fix: ajout de l'URL de sites-faciles dans le context-processor
    Accessible à tous les templates en cas de modification de liens.

--- impact/utils/context_processors.py
+++ impact/utils/context_processors.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 
 
-def custom_settings(request):
+def custom_settings(_):
     # allows direct access to some relevant settings
     return {
         "matomo_enabled": not settings.MATOMO_DISABLED,
         "sentry_dsn": settings.SENTRY_DSN,
         "sentry_env": settings.SENTRY_ENV,
+        "sites_faciles_base_url": settings.SITES_FACILES_BASE_URL,
     }
