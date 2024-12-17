986df070e7a8f11da33ea33850bd6723bfe86e3a
Stéphane <stephane@yaal.coop>
2024-12-17 16:58:17 +0100

    Paramétrage du domaine sur lequel les cookies s'appliquent
    Une variable d'environnement facultative permet de choisir sur
    quel domaine les cookies de tarteaucitron s'appliquent.
    Cela permet en production de les mettre sur l'ensemble du domaine
    portail-rse et donc qu'ils soient valides sur le service sites-facile
    ainsi que le service de ce dépôt (app.portail...).

--- impact/utils/context_processors.py
+++ impact/utils/context_processors.py
@@ -4,6 +4,7 @@ from django.conf import settings
 def custom_settings(_):
     # allows direct access to some relevant settings
     return {
+        "cookie_domain": settings.COOKIE_DOMAIN,
         "matomo_enabled": not settings.MATOMO_DISABLED,
         "sentry_dsn": settings.SENTRY_DSN,
         "sentry_env": settings.SENTRY_ENV,
