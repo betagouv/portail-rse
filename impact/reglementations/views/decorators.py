838900384dc30f5bfb58dd3beb417f5c6773ac38
Stéphane <stephane@yaal.coop>
2025-03-13 18:16:22 +0100

    vérification du propriétaire sur les documents IA

--- impact/reglementations/views/decorators.py
+++ impact/reglementations/views/decorators.py
@@ -3,6 +3,7 @@ from functools import wraps
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 
+from reglementations.models.csrd import DocumentAnalyseIA
 from reglementations.models.csrd import Enjeu
 from reglementations.models.csrd import RapportCSRD
 
@@ -31,5 +32,19 @@ def enjeu_required(function):
                 "L'utilisateur n'a pas les permissions nécessaires pour accéder à ce rapport CSRD"
             )
         return function(request, enjeu_id, *args, **kwargs)
+
+    return wrap
+
+
+def document_required(function):
+    @wraps(function)
+    def wrap(request, id_document, *args, **kwargs):
+        document = get_object_or_404(DocumentAnalyseIA, id=id_document)
+
+        if not document.rapport_csrd.modifiable_par(request.user):
+            raise PermissionDenied(
+                "L'utilisateur n'a pas les permissions nécessaires pour accéder à ce fichier"
+            )
+        return function(request, id_document, *args, **kwargs)
 
     return wrap
