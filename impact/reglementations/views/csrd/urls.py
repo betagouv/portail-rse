55525392321f988a0ba96d548386e65860ef0a32
emillumine <camille@yaal.coop>
2025-03-14 16:43:24 +0100

    refacto: découpe un gros fichier en deux

--- impact/reglementations/views/csrd/urls.py
+++ impact/reglementations/views/csrd/urls.py
@@ -1,70 +1,80 @@
 from django.urls import path
 from django.urls.conf import include
 
-from reglementations import views
+from reglementations.views.csrd.analyse_ia import ajout_document
+from reglementations.views.csrd.analyse_ia import etat_analyse_IA
+from reglementations.views.csrd.analyse_ia import lancement_analyse_IA
+from reglementations.views.csrd.analyse_ia import resultat_IA_xlsx
+from reglementations.views.csrd.analyse_ia import suppression_document
+from reglementations.views.csrd.analyse_ia import synthese_resultat_IA_xlsx
+from reglementations.views.csrd.csrd import datapoints_xlsx
+from reglementations.views.csrd.csrd import enjeux_materiels_xlsx
+from reglementations.views.csrd.csrd import enjeux_xlsx
+from reglementations.views.csrd.csrd import gestion_csrd
+from reglementations.views.csrd.csrd import guide_csrd
 
 urlpatterns = [
-    path("csrd", views.csrd.guide_csrd, name="csrd"),
-    path("csrd/guide/<str:siren>", views.csrd.guide_csrd, name="csrd"),
+    path("csrd", guide_csrd, name="csrd"),
+    path("csrd/guide/<str:siren>", guide_csrd, name="csrd"),
     path(
         "csrd/guide/<str:siren>/phase-<int:phase>",
-        views.csrd.guide_csrd,
+        guide_csrd,
         name="csrd_phase",
     ),
     path(
         "csrd/guide/<str:siren>/phase-<int:phase>/etape-<int:etape>",
-        views.csrd.guide_csrd,
+        guide_csrd,
         name="csrd_etape",
     ),
     path(
         "csrd/guide/<str:siren>/phase-<int:phase>/etape-<int:etape>-<int:sous_etape>",
-        views.csrd.guide_csrd,
+        guide_csrd,
         name="csrd_sous_etape",
     ),
     path(
         "csrd/<str:siren>/etape-<str:id_etape>",
-        views.csrd.gestion_csrd,
+        gestion_csrd,
         name="gestion_csrd",
     ),
-    path("csrd/<str:siren>/enjeux.xlsx", views.csrd.enjeux_xlsx, name="enjeux_xlsx"),
+    path("csrd/<str:siren>/enjeux.xlsx", enjeux_xlsx, name="enjeux_xlsx"),
     path(
         "csrd/<str:siren>/enjeux_materiels.xlsx",
-        views.csrd.enjeux_materiels_xlsx,
+        enjeux_materiels_xlsx,
         name="enjeux_materiels_xlsx",
     ),
     path(
         "csrd/<str:siren>/datapoints.xlsx",
-        views.csrd.datapoints_xlsx,
+        datapoints_xlsx,
         name="datapoints_xlsx",
     ),
     path(
         "csrd/<int:csrd_id>/ajout_document",
-        views.csrd.ajout_document,
+        ajout_document,
         name="ajout_document",
     ),
     path(
         "csrd/<int:id_document>/suppression",
-        views.csrd.suppression_document,
+        suppression_document,
         name="suppression_document",
     ),
     path(
         "ESRS-predict/<int:id_document>",
-        views.csrd.etat_analyse_IA,
+        etat_analyse_IA,
         name="etat_analyse_IA",
     ),
     path(
         "ESRS-predict/<int:id_document>/start",
-        views.csrd.lancement_analyse_IA,
+        lancement_analyse_IA,
         name="lancement_analyse_IA",
     ),
     path(
         "ESRS-predict/<int:id_document>/resultats.xlsx",
-        views.csrd.resultat_IA_xlsx,
+        resultat_IA_xlsx,
         name="resultat_IA_xlsx",
     ),
     path(
         "ESRS-predict/<int:csrd_id>/synthese_resultats.xlsx",
-        views.csrd.synthese_resultat_IA_xlsx,
+        synthese_resultat_IA_xlsx,
         name="synthese_resultats_IA",
     ),
 ]
