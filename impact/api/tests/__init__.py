2ddbdfa66cb2ed52f623c5563bc843c6d6c74225
Stéphane <stephane@yaal.coop>
2025-03-18 10:29:06 +0100

    refacto: test: réutilisation d'une classe Mock qui est aussi utilisée en dehors de api/

--- impact/api/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-class MockedResponse:
-    def __init__(self, status_code, json_content=""):
-        self.status_code = status_code
-        self.json_content = json_content
-
-    def json(self):
-        return self.json_content
