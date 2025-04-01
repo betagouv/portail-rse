69152275480717001a22a8065df04164ab8d1c5a
Stéphane <stephane@yaal.coop>
2025-04-01 18:33:20 +0200

    nettoyage: suppression des fiches réglementaires
    Les fiches réglementaires sont disponibles sur le site vitrine. Elles ne doivent plus être affichées dans la partie gestion. Aucun lien ne pointait encore vers ces URL sur le site de gestion.

--- impact/reglementations/urls.py
+++ impact/reglementations/urls.py
@@ -1,75 +1,10 @@
 from django.urls import path
 from django.urls.conf import include
-from django.views.generic.base import TemplateView
 
 from reglementations import views
 
 app_name = "reglementations"
 urlpatterns = [
-    path(
-        "reglementations/bdese",
-        TemplateView.as_view(template_name="reglementations/fiches/bdese.html"),
-        name="fiche_bdese",
-    ),
-    path(
-        "reglementations/index-egalite-professionnelle",
-        TemplateView.as_view(
-            template_name="reglementations/fiches/index-egalite-professionnelle.html",
-            extra_context={
-                "reglementation": views.index_egapro.IndexEgaproReglementation
-            },
-        ),
-        name="fiche_index_egapro",
-    ),
-    path(
-        "reglementations/dispositif-alerte",
-        TemplateView.as_view(
-            template_name="reglementations/fiches/dispositif-alerte.html"
-        ),
-        name="fiche_dispositif_alerte",
-    ),
-    path(
-        "reglementations/bilan-ges",
-        TemplateView.as_view(template_name="reglementations/fiches/bilan-ges.html"),
-        name="fiche_bilan_ges",
-    ),
-    path(
-        "reglementations/audit-energetique",
-        TemplateView.as_view(
-            template_name="reglementations/fiches/audit-energetique.html"
-        ),
-        name="fiche_audit_energetique",
-    ),
-    path(
-        "reglementations/dispositif-anticorruption",
-        TemplateView.as_view(
-            template_name="reglementations/fiches/dispositif-anticorruption.html"
-        ),
-        name="fiche_dispositif_anticorruption",
-    ),
-    path(
-        "reglementations/declaration-de-performance-extra-financiere",
-        TemplateView.as_view(template_name="reglementations/fiches/dpef.html"),
-        name="fiche_dpef",
-    ),
-    path(
-        "reglementations/rapport-durabilite-csrd",
-        TemplateView.as_view(
-            template_name="reglementations/fiches/csrd.html",
-            extra_context={"reglementation": views.csrd.CSRDReglementation},
-        ),
-        name="fiche_csrd",
-    ),
-    path(
-        "reglementations/plan-vigilance",
-        TemplateView.as_view(
-            template_name="reglementations/fiches/plan-vigilance.html",
-            extra_context={
-                "reglementation": views.plan_vigilance.PlanVigilanceReglementation
-            },
-        ),
-        name="fiche_plan_vigilance",
-    ),
     path(
         "tableau-de-bord",
         views.tableau_de_bord,
