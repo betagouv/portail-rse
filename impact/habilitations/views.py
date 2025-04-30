c587d7ef9530d5508e5b94557fe952d446741db9
Stéphane <stephane@yaal.coop>
2025-04-30 16:51:34 +0200

    affichage du rôle actuel  sur la page des membres d'une entreprise

--- impact/habilitations/views.py
+++ impact/habilitations/views.py
@@ -4,13 +4,17 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
+from .models import Habilitation
 from entreprises.models import Entreprise
 
 
 @login_required()
 def index(request, siren):
     entreprise = get_object_or_404(Entreprise, siren=siren)
-    context = {"entreprise": entreprise}
+    context = {
+        "entreprise": entreprise,
+        "habilitation": Habilitation.pour(entreprise, request.user),
+    }
 
     # organisation des membres par habilitations
     habilitations = defaultdict(list)
