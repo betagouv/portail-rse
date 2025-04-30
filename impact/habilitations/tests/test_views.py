640cc20f55b30f23f2cf45a8f1c4e1aea95102d7
Stéphane <stephane@yaal.coop>
2025-04-30 18:46:56 +0200

    apparition du formulaire d'invitation

--- impact/habilitations/tests/test_views.py
+++ impact/habilitations/tests/test_views.py
@@ -1,3 +1,5 @@
+import html
+
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
@@ -28,3 +30,22 @@ def test_page_membres_d_une_entreprise_nécessite_d_être_connecté(
 
     redirect_url = f"""{reverse("users:login")}?next={url}"""
     assert response.redirect_chain == [(redirect_url, 302)]
+
+
+def test_invitation_a_devenir_membre(client, alice, entreprise_factory):
+    entreprise = entreprise_factory()
+    Habilitation.ajouter(entreprise, alice, fonctions="Présidente")
+    client.force_login(alice)
+    data = {
+        "email": "bob@bob.test",
+    }
+    url = f"/droits/{entreprise.siren}"
+
+    response = client.post(url, data=data, follow=True)
+
+    redirect_url = (
+        f"""{reverse("habilitations:membres_entreprise", args=[entreprise.siren])}"""
+    )
+    assert response.redirect_chain == [(redirect_url, 302)]
+    content = html.unescape(response.content.decode("utf-8"))
+    assert "L'invitation a été envoyée." in content
--- impact/habilitations/views.py
+++ impact/habilitations/views.py
@@ -1,9 +1,13 @@
 from collections import defaultdict
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from .forms import InvitationForm
 from .models import Habilitation
 from entreprises.models import Entreprise
 
@@ -11,9 +15,18 @@ from entreprises.models import Entreprise
 @login_required()
 def index(request, siren):
     entreprise = get_object_or_404(Entreprise, siren=siren)
+    if request.POST:
+        messages.success(
+            request,
+            "L'invitation a été envoyée.",
+        )
+        return redirect(
+            reverse("habilitations:membres_entreprise", args=[entreprise.siren])
+        )
     context = {
         "entreprise": entreprise,
         "habilitation": Habilitation.pour(entreprise, request.user),
+        "form": InvitationForm(),
     }
 
     # organisation des membres par habilitations
