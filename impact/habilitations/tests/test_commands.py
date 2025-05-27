a5d7fab3e7f922ffdafdf33ce59bf1bde21621a7
Stéphane <stephane@yaal.coop>
2025-05-27 13:06:28 +0200

    prise en compte du cas de l'entreprise test pour les utilisateurs curieux
    Permet de garder le compte de support comme propriétaire sur l'entreprise et de le rendre visible à des utilisateurs voulant essayer le service avec l'entreprise test.

--- impact/habilitations/tests/test_commands.py
+++ impact/habilitations/tests/test_commands.py
@@ -1,9 +1,11 @@
 import pytest
+from django.conf import settings
 
 from habilitations.management.commands.supprime_utilisateurs_sur_entreprise_test import (
     Command,
 )
 from habilitations.models import Habilitation
+from users.models import User
 
 
 @pytest.mark.django_db(transaction=True)
@@ -23,6 +25,20 @@ def test_nettoie_entreprise_test_supprime_ses_utilisateurs(
     assert entreprise.habilitation_set.count() == 0
     autre_entreprise.refresh_from_db()
     assert autre_entreprise.habilitation_set.count() == 2
+
+
+@pytest.mark.django_db(transaction=True)
+def test_nettoie_entreprise_test_ne_supprime_pas_adresse_de_contact(
+    db, mocker, entreprise_factory
+):
+    entreprise = entreprise_factory(siren="000000001")
+    contact = User.objects.create(email=settings.SUPPORT_EMAIL)
+    Habilitation.ajouter(entreprise, contact)
+
+    Command().handle()
+
+    entreprise.refresh_from_db()
+    assert entreprise.habilitation_set.count() == 1
 
 
 @pytest.mark.django_db(transaction=True)
