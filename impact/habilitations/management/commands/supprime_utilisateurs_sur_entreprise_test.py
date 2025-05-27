224fd290dc6be74c74331a2581adb758cc780f75
Stéphane <stephane@yaal.coop>
2025-05-27 13:06:28 +0200

    prise en compte du cas de l'entreprise test pour les utilisateurs curieux
    Permet de garder le compte de support comme propriétaire sur l'entreprise et de le rendre visible à des utilisateurs voulant essayer le service avec l'entreprise test.

--- impact/habilitations/management/commands/supprime_utilisateurs_sur_entreprise_test.py
+++ impact/habilitations/management/commands/supprime_utilisateurs_sur_entreprise_test.py
@@ -1,6 +1,7 @@
 """
 Ce script permet de supprimer automatiquement tout compte qui s'y serait fait inviter pour tester le service.
 """
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from entreprises.models import Entreprise
@@ -10,4 +11,6 @@ from habilitations.models import Habilitation
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if entreprise := Entreprise.objects.filter(siren="000000001").first():
-            Habilitation.objects.filter(entreprise=entreprise).delete()
+            Habilitation.objects.filter(entreprise=entreprise).exclude(
+                user__email=settings.SUPPORT_EMAIL
+            ).delete()
--- impact/utils/emails.py
+++ impact/utils/emails.py
@@ -1,4 +1,9 @@
+from django.conf import settings
+
+
 def cache_partiellement_un_email(email):
+    if email == settings.SUPPORT_EMAIL:
+        return email
     nom, domaine = email.split("@")
     etoiles = "*" * (len(nom) - 2)
     return f"{nom[0]}{etoiles}{nom[-1]}@{domaine}"
