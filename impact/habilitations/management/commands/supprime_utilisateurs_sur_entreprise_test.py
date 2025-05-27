d61c06fb565be8a8856926e58c25b59b19eac537
Stéphane <stephane@yaal.coop>
2025-05-27 14:01:26 +0200

    force l'utilisateur de support à être propriétaire de l'entreprise test

--- impact/habilitations/management/commands/supprime_utilisateurs_sur_entreprise_test.py
+++ impact/habilitations/management/commands/supprime_utilisateurs_sur_entreprise_test.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand
 
 from entreprises.models import Entreprise
 from habilitations.models import Habilitation
+from users.models import User
 
 
 class Command(BaseCommand):
@@ -14,3 +15,10 @@ class Command(BaseCommand):
             Habilitation.objects.filter(entreprise=entreprise).exclude(
                 user__email=settings.SUPPORT_EMAIL
             ).delete()
+
+            # dans le cas où un testeur supprime l'utilisateur contact de l'entreprise, le réajouter
+            if not Habilitation.objects.filter(
+                entreprise=entreprise, user__email=settings.SUPPORT_EMAIL
+            ):
+                contact = User.objects.get(email=settings.SUPPORT_EMAIL)
+                Habilitation.ajouter(entreprise, contact, fonctions="Démo")
