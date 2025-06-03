c9d3bcef72d4ec019ed0d57f7b0c89d87cc776d0
Stéphane <stephane@yaal.coop>
2025-06-03 15:15:04 +0200

    refacto: renommage de paramètres pour expliciter leur généricité

--- impact/utils/tokens.py
+++ impact/utils/tokens.py
@@ -1,5 +1,8 @@
 """
 La génération de token est une version simplifiée de django.contrib.auth.tokens.PasswordResetTokenGenerator.
+
+Le paramètre `db_object` est une instance héritant de `django.db.models.Model` et ayant un attribut `email`.
+Exemples : les instances de User et Invitation.
 """
 from django.conf import settings
 from django.utils.crypto import constant_time_compare
@@ -8,19 +11,19 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
 
-def make_token(user, key_salt):
+def make_token(db_object, key_salt):
     hash_string = salted_hmac(
         key_salt,
-        f"{user.pk}{user.email}",
+        f"{db_object.pk}{db_object.email}",
         secret=settings.SECRET_KEY,
         algorithm="sha256",
     ).hexdigest()[::2]
     return hash_string
 
 
-def check_token(user, key_salt, token):
-    return constant_time_compare(make_token(user, key_salt), token)
+def check_token(db_object, key_salt, token):
+    return constant_time_compare(make_token(db_object, key_salt), token)
 
 
-def uidb64(user):
-    return urlsafe_base64_encode(force_bytes(user.pk))
+def uidb64(db_object):
+    return urlsafe_base64_encode(force_bytes(db_object.pk))
