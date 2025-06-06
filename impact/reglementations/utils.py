5397b73a0e3a17feca52379319ee15cd1db7b48e
Frédéric Vergez <fred@ikarius.com>
2025-06-06 09:37:22 +0200

    feat(WIP): ajout des VSME au tableau de bord
    La classe de règlementation est à affiner / discuter.

---
+++ impact/reglementations/utils.py
@@ -0,0 +1,76 @@
+from django.conf import settings
+from django.urls.base import reverse_lazy
+
+from entreprises.models import CaracteristiquesAnnuelles
+from reglementations.views.base import Reglementation
+from reglementations.views.base import ReglementationAction
+from reglementations.views.base import ReglementationStatus
+
+
+"""
+utils:
+- Module très mal nommé en attendant de réorganiser la structure de l'app `reglementations`
+- contient pour l'instant les différentes classes ou fonctions utilitaires pour les VSME
+"""
+
+# Modèles non-ORM :
+# Les modèles de traitement des réglementations (non-ORM),
+# devraient être situés "proches" des réglementations concernées.
+# Difficile dans le cas des VSME (import cyclique).
+# A moyen|long-terme, ces classes utilitaires devraient être placées ailleurs
+# que dans les vues (et probablement dans une autre app ou package).
+
+
+# TODO: à complèter et adapter après discussion
+class VSMEReglementation(Reglementation):
+    title = "Standard volontaire européen - VSME"
+    more_info_url = "https://portail-rse.beta.gouv.fr/csrd/vsme-pme/"
+    tag = "tag-social"
+    summary = "TODO: description des VSME"
+    zone = "europe"
+
+    @classmethod
+    def est_suffisamment_qualifiee(cls, caracteristiques):
+        return caracteristiques.effectif is not None
+
+    @classmethod
+    def criteres_remplis(cls, caracteristiques):
+        return []
+
+    @classmethod
+    def est_soumis(cls, caracteristiques):
+        return False
+
+    @classmethod
+    def calculate_status(
+        cls,
+        caracteristiques: CaracteristiquesAnnuelles,
+        user: settings.AUTH_USER_MODEL,
+    ) -> ReglementationStatus:
+        if reglementation_status := super().calculate_status(caracteristiques, user):
+            return reglementation_status
+
+        primary_action = ReglementationAction(
+            reverse_lazy(
+                "vsme:etape_vsme",
+                kwargs={
+                    "siren": caracteristiques.entreprise.siren,
+                    "etape": "introduction",
+                },
+            ),
+            "Standard volontaire européen - VSME",
+        )
+
+        return ReglementationStatus(
+            status=ReglementationStatus.STATUS_NON_SOUMIS,
+            status_detail="Vous n'êtes pas soumis à cette réglementation.",
+            primary_action=primary_action,
+        )
+
+    @classmethod
+    def calculate_status_for_anonymous_user(
+        cls, caracteristiques: CaracteristiquesAnnuelles
+    ):
+        return super().calculate_status_for_anonymous_user(
+            caracteristiques, primary_action=cls.NON_SOUMIS_PRIMARY_ACTION
+        )
