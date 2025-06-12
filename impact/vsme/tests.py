e6ae1099984b4d980c3f6f5809df2ce89a7bec8f
Stéphane <stephane@yaal.coop>
2025-06-12 17:21:10 +0200

    guillemets possibles dans les f-strings
    avec une version de black récente

--- impact/vsme/tests.py
+++ impact/vsme/tests.py
@@ -31,11 +31,11 @@ def test_acces_vsme_refuse(client, entreprise_qualifiee, bob):
 def test_chargement_templates(etape, client, entreprise_qualifiee, alice):
     # vérifie que tous les templates sont bien chargés dans la page résultante
     client.force_login(alice)
+
     response = client.get(f"/vsme/{entreprise_qualifiee.siren}/{etape}", follow=True)
+
     assertTemplateUsed(response, "base.html")
     assertTemplateUsed(response, "snippets/entete_page.html")
     assertTemplateUsed(response, "snippets/entete.html")
     assertTemplateUsed(response, "snippets/menu.html")
-
-    # note : pénible black et sa mauvaise gestion du parsing des f-string
-    assertTemplateUsed(response, f"etapes/{etape.replace('_','-')}.html")
+    assertTemplateUsed(response, f"etapes/{etape.replace("_","-")}.html")
