03d78fb85fe16a157f8402485b46477dc8834c9c
Frédéric Vergez <fred@ikarius.com>
2025-06-27 12:33:34 +0200

    fix: ajout d'un commentaire
    Pour expliquer l'import bizarrement placé de la class EventLog

--- impact/logs/event.py
+++ impact/logs/event.py
@@ -8,6 +8,8 @@ class EventLogHandler(logging.Handler):
         if record.args and isinstance(record.args, dict):
             payload |= record.args
 
+        # les logs sont définis avant le démarrage des apps django
+        # l'import ne doit pas être fait en entête...
         from .models import EventLog
 
         EventLog(level=record.levelno, msg=record.msg, payload=payload).save()
