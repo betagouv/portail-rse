9d310f9729eb810a45ccd08ad76f092d5ae813e6
emillumine <camille@yaal.coop>
2025-07-24 10:23:11 +0200

    modifie le contenu/affichage de certains éléments mineures

--- impact/habilitations/forms.py
+++ impact/habilitations/forms.py
@@ -6,7 +6,9 @@ from utils.forms import DsfrForm
 
 class InvitationForm(DsfrForm):
     email = forms.EmailField(
-        label="Adresse e-mail", widget=forms.TextInput(attrs={"autofocus": False})
+        label="Adresse e-mail",
+        help_text="La personne recevra un e-mail d'invitation pour créer son compte et rejoindre l'entreprise YAAL COOP. Si elle possède déjà un compte utilisateur sur le Portail RSE, elle sera directement ajoutée dans les contributeurs de l'entreprise.",
+        widget=forms.TextInput(attrs={"autofocus": False}),
     )
     role = forms.ChoiceField(
         label="Rôle du futur membre",
