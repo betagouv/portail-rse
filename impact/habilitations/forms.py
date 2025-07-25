1adffb2a56ebb15bfb368d54fd19d3d0e1953864
emillumine <camille@yaal.coop>
2025-07-25 11:17:55 +0200

    fix: supprime un nom d'entreprise écrit en dur :)

--- impact/habilitations/forms.py
+++ impact/habilitations/forms.py
@@ -7,7 +7,7 @@ from utils.forms import DsfrForm
 class InvitationForm(DsfrForm):
     email = forms.EmailField(
         label="Adresse e-mail",
-        help_text="La personne recevra un e-mail d'invitation pour créer son compte et rejoindre l'entreprise YAAL COOP. Si elle possède déjà un compte utilisateur sur le Portail RSE, elle sera directement ajoutée dans les contributeurs de l'entreprise.",
+        help_text="La personne recevra un e-mail d'invitation pour créer son compte et rejoindre l'entreprise. Si elle possède déjà un compte utilisateur sur le Portail RSE, elle sera directement ajoutée dans les contributeurs de l'entreprise.",
         widget=forms.TextInput(attrs={"autofocus": False}),
     )
     role = forms.ChoiceField(
