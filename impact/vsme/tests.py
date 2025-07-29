8d4e8cc5be1fe8ff078c019b54d8dfcb9ddecfb0
emillumine <camille@yaal.coop>
2025-07-29 12:30:07 +0200

    ajoute des urls génériques sans numéro siren pour accéder aux étapes de l'espace VSME

--- impact/vsme/tests.py
+++ impact/vsme/tests.py
@@ -1,11 +1,12 @@
 import pytest
+from django.contrib.messages import WARNING
+from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
 
 @pytest.fixture
 def entreprise_qualifiee(entreprise_factory, alice):
-    entreprise = entreprise_factory()
-    entreprise.users.add(alice)
+    entreprise = entreprise_factory(utilisateur=alice)
     return entreprise
 
 
@@ -39,3 +40,37 @@ def test_chargement_templates(etape, client, entreprise_qualifiee, alice):
     assertTemplateUsed(response, "snippets/entete.html")
     assertTemplateUsed(response, "snippets/menu.html")
     assertTemplateUsed(response, f"etapes/{etape.replace("_","-")}.html")
+
+
+@pytest.mark.parametrize("etape", ["introduction", "module_base", "module_narratif"])
+def test_siren_absent_redirige_vers_celui_de_l_entreprise_courante(
+    etape, client, entreprise_factory, alice
+):
+    entreprise = entreprise_factory(utilisateur=alice)
+    client.force_login(alice)
+    url = f"/vsme/{etape}/"
+
+    response = client.get(url, follow=True)
+
+    assert response.status_code == 200
+    redirect_url = reverse("vsme:etape_vsme", args=[entreprise.siren, etape])
+    assert response.redirect_chain == [(redirect_url, 302)]
+
+
+@pytest.mark.parametrize("etape", ["introduction", "module_base", "module_narratif"])
+def test_siren_absent_redirige_vers_l_ajout_d_entreprise_si_pas_d_entreprise(
+    etape, client, alice
+):
+    client.force_login(alice)
+    url = f"/vsme/{etape}/"
+
+    response = client.get(url, follow=True)
+
+    assert response.status_code == 200
+    assert response.redirect_chain == [(reverse("entreprises:entreprises"), 302)]
+    messages = list(response.context["messages"])
+    assert messages[0].level == WARNING
+    assert (
+        messages[0].message
+        == "Commencez par ajouter une entreprise à votre compte utilisateur avant d'accéder à l'espace VSME"
+    )
--- impact/vsme/urls.py
+++ impact/vsme/urls.py
@@ -5,6 +5,11 @@ import vsme.views as views
 app_name = "vsme"
 urlpatterns = [
     path(
+        "vsme/<str:etape>/",
+        views.etape_vsme,
+        name="etape_vsme",
+    ),
+    path(
         "vsme/<str:siren>/<str:etape>/",
         views.etape_vsme,
         name="etape_vsme",
