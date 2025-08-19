c8b4833dcf6d97c5265cdf94ed9ae904a004a467
Stéphane <stephane@yaal.coop>
2025-08-19 16:38:27 +0200

    les habilitations des utilisateurs associés à l'entreprise test sont supprimées au bout de 30 jours

--- impact/habilitations/management/commands/supprime_utilisateurs_sur_entreprise_test.py
+++ impact/habilitations/management/commands/supprime_utilisateurs_sur_entreprise_test.py
@@ -1,6 +1,10 @@
 """
 Ce script permet de supprimer automatiquement tout compte qui s'y serait fait inviter pour tester le service.
 """
+
+from datetime import date
+from datetime import timedelta
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -8,13 +12,18 @@ from entreprises.models import Entreprise
 from habilitations.models import Habilitation
 from users.models import User
 
+MAX_JOURS_HABILITATION = 30
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if entreprise := Entreprise.objects.filter(siren="000000001").first():
-            Habilitation.objects.filter(entreprise=entreprise).exclude(
-                user__email=settings.SUPPORT_EMAIL
-            ).delete()
+            date_min_habilitation = date.today() - timedelta(
+                days=MAX_JOURS_HABILITATION
+            )
+            Habilitation.objects.filter(
+                entreprise=entreprise, created_at__lt=date_min_habilitation
+            ).exclude(user__email=settings.SUPPORT_EMAIL).delete()
 
             # dans le cas où un testeur supprime l'utilisateur contact de l'entreprise, le réajouter
             if not Habilitation.objects.filter(
