2a9042d7f8d3e2df139616eb20355ae528f0067b
Stéphane <stephane@yaal.coop>
2025-08-20 14:05:44 +0200

    cache le nom et prénom des membres de l'entreprise test

--- impact/utils/tests/test_anonymise.py
+++ impact/utils/tests/test_anonymise.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 
 from utils.anonymisation import cache_partiellement_un_email
+from utils.anonymisation import cache_partiellement_un_mot
 
 
-def test():
+def test_email():
     assert cache_partiellement_un_email("alice@domain.test") == "a***e@domain.test"
     assert (
         cache_partiellement_un_email(settings.SUPPORT_EMAIL) == settings.SUPPORT_EMAIL
     )
+
+
+def test_mot():
+    assert cache_partiellement_un_mot("alice") == "a***e"
+    assert cache_partiellement_un_mot("carole") == "c****e"
