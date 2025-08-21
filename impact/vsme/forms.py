be690f9c3a49e1a1be3c9616b3469c793a2ac482
Stéphane <stephane@yaal.coop>
2025-08-21 17:38:57 +0200

    ajout formulaire liste

--- impact/vsme/forms.py
+++ impact/vsme/forms.py
@@ -4,15 +4,45 @@ from utils.forms import DsfrForm
 
 
 EXIGENCES_DE_PUBLICATION = [
-    "indicateur_nombre",
-    "indicateur_nombre",
+    {"type": "indicateur_nombre"},
+    {
+        "type": "indicateur_liste",
+        "contient": [
+            {
+                "label": "Nom de la filiale",
+                "clef": "nom",
+                "type": "texte",
+            },
+            {
+                "label": "Adresse",
+                "clef": "adresse",
+                "type": "nombre",
+            },
+        ],
+    },
 ]
+
+
+class DsfrCharField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        if isinstance(self.widget, forms.widgets.Select):
+            fr_name = "fr-select"
+        else:
+            fr_name = "fr-input"
+        self.widget.attrs.update({"class": f"{fr_name}"})
+        if name in self.errors:
+            self.widget.attrs.update(
+                {
+                    "class": f"{fr_name} {fr_name}-error",
+                    "aria-describedby": f"{name}-error-desc-error",
+                }
+            )
 
 
 def forms_par_exigence_de_publication(posted_data):
     _forms = []
 
-    class FormTableau(DsfrForm):
+    class FormListe(DsfrForm):
         pertinent = forms.BooleanField()
         indicateur_id = forms.IntegerField()
 
@@ -39,10 +69,10 @@ def forms_par_exigence_de_publication(posted_data):
         #        kwargs["initial"].update(fetched_posted_data)
         #    super().__init__(*args, instance=indicateur, **kwargs)
 
-    for index, type_indicateur in enumerate(EXIGENCES_DE_PUBLICATION, start=1):
+    for index, indicateur in enumerate(EXIGENCES_DE_PUBLICATION, start=1):
         # fk_indicateur = vsme._meta.get_field(field_name)
         # indicateur = fk_indicateur.related_model
-        # type_indicateur = indicateur.type
+        type_indicateur = indicateur["type"]
         if type_indicateur == "indicateur_nombre":
             if posted_data and int(posted_data["indicateur_id"][0]) == index:
                 form = FormNombre(data=posted_data)
@@ -54,8 +84,21 @@ def forms_par_exigence_de_publication(posted_data):
             #    form=FormNombre,
             #    exclude=[],
             # )
-        elif type_indicateur == "indicateur_tableau":
-            form = FormTableau(initial={"indicateur_id": index})
+        elif type_indicateur == "indicateur_liste":
+            form = FormListe(initial={"indicateur_id": index})
+            for contenu in indicateur["contient"]:
+                label = contenu["label"]
+                clef = contenu["clef"]
+                if contenu["type"] == "texte":
+                    form.fields[clef] = forms.CharField(
+                        label=label, widget=forms.TextInput(attrs={"class": "fr-input"})
+                    )
+                else:
+                    form.fields[clef] = forms.IntegerField(
+                        label=label,
+                        widget=forms.NumberInput(attrs={"class": "fr-input"}),
+                    )
+
             # forms.modelform_factory(
             #    indicateur,
             #    form=FormTableau,
