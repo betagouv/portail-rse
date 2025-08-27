6eff0b9ac14a9a93b8ec3005b760be65f224658b
Stéphane <stephane@yaal.coop>
2025-08-27 16:03:28 +0200

    prototype de la saisie des indicateurs VSME
    avec indicateurs en session

--- impact/utils/forms.py
+++ impact/utils/forms.py
@@ -4,6 +4,9 @@ from django import forms
 class DsfrForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.reinitialise_widgets()
+
+    def reinitialise_widgets(self):
         for name, field in self.fields.items():
             if isinstance(field.widget, forms.widgets.Select):
                 fr_name = "fr-select"
@@ -17,6 +20,13 @@ class DsfrForm(forms.Form):
                         "aria-describedby": f"{name}-error-desc-error",
                     }
                 )
+
+
+class DsfrFormSet(forms.BaseFormSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for form in self.forms:
+            form.reinitialise_widgets()
 
 
 class DateInput(forms.DateInput):
