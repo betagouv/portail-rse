95d2f3f03062b41a38f3e42cc7daff8f4bed1ae6
Stéphane <stephane@yaal.coop>
2025-08-28 18:22:45 +0200

    refacto: if elif en match case

--- impact/vsme/factory.py
+++ impact/vsme/factory.py
@@ -28,66 +28,71 @@ def create_form_from_yaml(yaml_data, indicateur_id):
         }
 
         # Gestion des arguments spécifiques au type de champ
-        if field_type == "text":
-            field_kwargs["max_length"] = field.get("max_length", 255)
-            field_kwargs["widget"] = forms.TextInput(
-                attrs={"class": "fr-input"},
-            )
-            _DynamicForm.base_fields[field_name] = forms.CharField(**field_kwargs)
-        elif field_type == "email":
-            field_kwargs["widget"] = forms.TextInput(
-                attrs={"class": "fr-input"},
-            )
-            _DynamicForm.base_fields[field_name] = forms.EmailField(**field_kwargs)
-        elif field_type == "number":
-            field_kwargs["min_value"] = field.get("min")
-            field_kwargs["max_value"] = field.get("max")
-            field_kwargs["widget"] = forms.NumberInput(
-                attrs={"class": "fr-input"},
-            )
-            _DynamicForm.base_fields[field_name] = forms.IntegerField(**field_kwargs)
-        elif field_type == "boolean":
-            _DynamicForm.base_fields[field_name] = forms.BooleanField(**field_kwargs)
-        elif field_type == "choice":
-            field_kwargs["choices"] = (
-                (choice["name"], choice["label"]) for choice in field["choices"]
-            )
-            field_kwargs["widget"] = forms.widgets.Select(
-                attrs={"class": "fr-select"},
-            )
-            _DynamicForm.base_fields[field_name] = forms.ChoiceField(**field_kwargs)
-        elif field_type == "table":
+        match field_type:
+            case "text":
+                field_kwargs["max_length"] = field.get("max_length", 255)
+                field_kwargs["widget"] = forms.TextInput(
+                    attrs={"class": "fr-input"},
+                )
+                _DynamicForm.base_fields[field_name] = forms.CharField(**field_kwargs)
+            case "email":
+                field_kwargs["widget"] = forms.TextInput(
+                    attrs={"class": "fr-input"},
+                )
+                _DynamicForm.base_fields[field_name] = forms.EmailField(**field_kwargs)
+            case "number":
+                field_kwargs["min_value"] = field.get("min")
+                field_kwargs["max_value"] = field.get("max")
+                field_kwargs["widget"] = forms.NumberInput(
+                    attrs={"class": "fr-input"},
+                )
+                _DynamicForm.base_fields[field_name] = forms.IntegerField(
+                    **field_kwargs
+                )
+            case "boolean":
+                _DynamicForm.base_fields[field_name] = forms.BooleanField(
+                    **field_kwargs
+                )
+            case "choice":
+                field_kwargs["choices"] = (
+                    (choice["name"], choice["label"]) for choice in field["choices"]
+                )
+                field_kwargs["widget"] = forms.widgets.Select(
+                    attrs={"class": "fr-select"},
+                )
+                _DynamicForm.base_fields[field_name] = forms.ChoiceField(**field_kwargs)
+            case "table":
 
-            class TableauFormSet(forms.BaseFormSet):
-                def add_fields(self, form, index):
-                    super().add_fields(form, index)
-                    for column in field["columns"]:
-                        label = column["label"]
-                        name = column["name"]
-                        if column["type"] == "text":
-                            form.fields[name] = forms.CharField(
-                                label=label,
-                                widget=forms.TextInput(
-                                    attrs={"class": "fr-input"},
-                                ),
-                                required=False,
-                            )
-                        elif column["type"] == "number":
-                            form.fields[name] = forms.IntegerField(
-                                label=label,
-                                widget=forms.NumberInput(
-                                    attrs={"class": "fr-input"},
-                                ),
-                                required=False,
-                            )
-                        else:
-                            raise Exception("Typo")
+                class TableauFormSet(forms.BaseFormSet):
+                    def add_fields(self, form, index):
+                        super().add_fields(form, index)
+                        for column in field["columns"]:
+                            label = column["label"]
+                            name = column["name"]
+                            if column["type"] == "text":
+                                form.fields[name] = forms.CharField(
+                                    label=label,
+                                    widget=forms.TextInput(
+                                        attrs={"class": "fr-input"},
+                                    ),
+                                    required=False,
+                                )
+                            elif column["type"] == "number":
+                                form.fields[name] = forms.IntegerField(
+                                    label=label,
+                                    widget=forms.NumberInput(
+                                        attrs={"class": "fr-input"},
+                                    ),
+                                    required=False,
+                                )
+                            else:
+                                raise Exception("Typo")
 
-            FormSet = forms.formset_factory(
-                _DynamicForm, formset=TableauFormSet, extra=1
-            )
-            FormSet.indicator_type = "table"
-            return FormSet
+                FormSet = forms.formset_factory(
+                    _DynamicForm, formset=TableauFormSet, extra=1
+                )
+                FormSet.indicator_type = "table"
+                return FormSet
         # et on continue ...
 
     return _DynamicForm
