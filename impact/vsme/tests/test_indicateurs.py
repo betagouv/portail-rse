78a2840780c66276e5b03318de8b87a02c466647
emillumine <camille@yaal.coop>
2025-09-24 10:15:28 +0200

    fixup modifie et uniformise les urls d'accès - correction des tests

---
+++ impact/vsme/tests/test_indicateurs.py
@@ -0,0 +1,47 @@
+import pytest
+from pytest_django.asserts import assertTemplateUsed
+
+INDICATEURS_VSME_URL = "/indicateurs/vsme/{siren}"
+CATEGORIE_VSME_URL = "/indicateurs/vsme/{vsme_id}/categorie/{categorie_id}"
+
+
+def test_page_indicateurs_vsme(client, entreprise_qualifiee, alice):
+    client.force_login(alice)
+
+    url = INDICATEURS_VSME_URL.format(siren=entreprise_qualifiee.siren)
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assertTemplateUsed(response, "base.html")
+    assertTemplateUsed(response, "snippets/tableau_de_bord_menu.html")
+    assertTemplateUsed(response, "vsme/indicateurs.html")
+
+
+@pytest.mark.parametrize(
+    "categorie", ["informations-generales", "environnement", "social", "gouvernance"]
+)
+def test_chargement_templates_des_categories_de_vsme(
+    categorie, client, alice, rapport_vsme
+):
+    # vérifie que tous les templates sont bien chargés dans la page résultante
+    client.force_login(alice)
+
+    url = CATEGORIE_VSME_URL.format(vsme_id=rapport_vsme.id, categorie_id=categorie)
+    response = client.get(url, follow=True)
+
+    assert response.status_code == 200
+    assertTemplateUsed(response, "base.html")
+    assertTemplateUsed(response, "snippets/tableau_de_bord_menu.html")
+    assertTemplateUsed(response, f"categories/{categorie}.html")
+
+
+def test_categorie_de_vsme_inexistante_retourne_une_404(client, alice, rapport_vsme):
+    # vérifie que tous les templates sont bien chargés dans la page résultante
+    client.force_login(alice)
+
+    url = CATEGORIE_VSME_URL.format(
+        vsme_id=rapport_vsme.id, categorie_id="n-existe-pas"
+    )
+    response = client.get(url, follow=True)
+
+    assert response.status_code == 404
