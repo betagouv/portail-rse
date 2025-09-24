7578881b393aa06c076c35aa6a588aa7dc53e27f
Stéphane <stephane@yaal.coop>
2025-09-24 17:24:49 +0200

    wip: indicateur de progression pour les catégories

--- impact/vsme/templatetags/progression_vsme.py
+++ impact/vsme/templatetags/progression_vsme.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def progression_exigence(rapport_vsme, exigence_de_publication):
     return rapport_vsme.progression_par(exigence_de_publication)
+
+
+@register.simple_tag
+def progression_categorie(rapport_vsme, categorie_id):
+    return 27
