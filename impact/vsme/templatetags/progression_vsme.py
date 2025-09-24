78688ccdeacc95d792130c0f043f9603c4ac4002
Stéphane <stephane@yaal.coop>
2025-09-24 17:11:43 +0200

    fixup: renammage tag (a appliquer sur calcul de l'avancement d'une exigence de publication)

--- impact/vsme/templatetags/progression_vsme.py
+++ impact/vsme/templatetags/progression_vsme.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 
 @register.simple_tag
-def progression_vsme(rapport_vsme, exigence_de_publication):
+def progression_exigence(rapport_vsme, exigence_de_publication):
     return rapport_vsme.progression_par(exigence_de_publication)
