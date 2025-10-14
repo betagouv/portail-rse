9c20145d75392e495a9a23fc1f1b693f33f5b3b3
Stéphane <stephane@yaal.coop>
2025-10-14 17:59:15 +0200

    admin: affichage des indicateurs liés à un rapport VSME

--- impact/vsme/admin.py
+++ impact/vsme/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 
+from .models import Indicateur
 from .models import RapportVSME
+
+
+class IndicateurInline(admin.TabularInline):
+    model = Indicateur
+    readonly_fields = (
+        "schema_id",
+        "schema_version",
+        "data",
+    )
+    extra = 0
 
 
 class RapportVSMEAdmin(admin.ModelAdmin):
     raw_id_fields = ("entreprise",)
+    inlines = (IndicateurInline,)
 
 
 admin.site.register(RapportVSME, RapportVSMEAdmin)
