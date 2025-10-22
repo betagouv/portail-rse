5efe1e8be26a7425ac02b757940e86356970c22b
emillumine <camille@yaal.coop>
2025-10-22 16:37:33 +0200

    tests: refacto pour simplifier des tests

--- impact/vsme/tests/test_extra_validators.py
+++ impact/vsme/tests/test_extra_validators.py
@@ -1,55 +1,57 @@
-from conftest import CODE_SA
+import pytest
+
 from vsme.forms import create_multiform_from_schema
 from vsme.models import Indicateur
 from vsme.views import load_indicateur_schema
 
 
-def test_succès_vérification_total_des_salariés(client, rapport_vsme):
-    rapport_vsme.entreprise.categorie_juridique_sirene = CODE_SA
+@pytest.fixture
+def rapport_vsme_42_salariés(rapport_vsme):
+    # Indicateur nombre salariés
     Indicateur.objects.create(
         rapport_vsme=rapport_vsme,
         schema_id="B1-24-e-v",
         data={"methode_comptabilisation": "ETP", "nombre_salaries": 42},
     )
+    # Indicateur pays
     Indicateur.objects.create(
         rapport_vsme=rapport_vsme,
         schema_id="B1-24-e-vi",
         data={"pays": ["FIN", "FRA"]},
     )
-    indicateur_schema = load_indicateur_schema("B8-39-c")
-    multiform_class = create_multiform_from_schema(indicateur_schema, rapport_vsme)
+    return rapport_vsme
+
+
+def test_succès_vérification_total_des_salariés(client, rapport_vsme_42_salariés):
+    indicateur_effectifs_pays_schema = load_indicateur_schema("B8-39-c")
+    multiform_class = create_multiform_from_schema(
+        indicateur_effectifs_pays_schema, rapport_vsme_42_salariés
+    )
 
     data = {
         "form-TOTAL_FORMS": "2",
         "form-INITIAL_FORMS": "2",
         "form-0-nombre_salaries": "20",
         "form-1-nombre_salaries": "22",
+        # total nombre salariés égal à 42
     }
     multiform = multiform_class(data)
 
     assert multiform.is_valid()
 
 
-def test_échec_vérification_total_des_salariés(client, rapport_vsme):
-    rapport_vsme.entreprise.categorie_juridique_sirene = CODE_SA
-    Indicateur.objects.create(
-        rapport_vsme=rapport_vsme,
-        schema_id="B1-24-e-v",
-        data={"methode_comptabilisation": "ETP", "nombre_salaries": 42},
+def test_échec_vérification_total_des_salariés(client, rapport_vsme_42_salariés):
+    indicateur_effectifs_pays_schema = load_indicateur_schema("B8-39-c")
+    multiform_class = create_multiform_from_schema(
+        indicateur_effectifs_pays_schema, rapport_vsme_42_salariés
     )
-    Indicateur.objects.create(
-        rapport_vsme=rapport_vsme,
-        schema_id="B1-24-e-vi",
-        data={"pays": ["FIN", "FRA"]},
-    )
-    indicateur_schema = load_indicateur_schema("B8-39-c")
-    multiform_class = create_multiform_from_schema(indicateur_schema, rapport_vsme)
 
     data = {
         "form-TOTAL_FORMS": "2",
         "form-INITIAL_FORMS": "2",
         "form-0-nombre_salaries": "1",
         "form-1-nombre_salaries": "3",
+        # total nombre salariés différent de 42
     }
     multiform = multiform_class(data)
 
