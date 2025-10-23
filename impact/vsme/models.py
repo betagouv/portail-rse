5e7a909405d4125b8c8ec5ac13fe6e2fa6a042b1
emillumine <camille@yaal.coop>
2025-10-23 11:44:00 +0200

    ajoute le schema des indicateurs de B10

--- impact/vsme/models.py
+++ impact/vsme/models.py
@@ -125,6 +125,7 @@ EXIGENCES_DE_PUBLICATION = {
         "Effectifs : rémunération, négociation collective et formation",
         Categorie.SOCIAL,
         "https://portail-rse.beta.gouv.fr/vsme/b10-personnel-remuneration-negociation-collective-et-formation/",
+        remplissable=True,
     ),
     "B11": ExigenceDePublication(
         "B11",
