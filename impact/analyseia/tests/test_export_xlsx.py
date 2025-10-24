126acecbcfadf9f3713a647d08c92f4a18b9d402
Stéphane <stephane@yaal.coop>
2025-10-24 16:39:56 +0200

    refacto: factorisation des routes pour la génération des fichiers xlsx

--- impact/analyseia/tests/test_export_xlsx.py
+++ impact/analyseia/tests/test_export_xlsx.py
@@ -1,13 +1,15 @@
 from io import BytesIO
 
+import pytest
 from django.urls import reverse
 from openpyxl import load_workbook
 
 from analyseia.models import AnalyseIA
 
 
+@pytest.mark.parametrize("rendu", ["theme", "esrs"])
 def test_telechargement_des_resultats_IA_d_un_document_au_format_xlsx(
-    client, entreprise_factory, alice
+    rendu, client, entreprise_factory, alice
 ):
     entreprise = entreprise_factory(utilisateur=alice)
     document = AnalyseIA.objects.create(
@@ -35,7 +37,7 @@ def test_telechargement_des_resultats_IA_d_un_document_au_format_xlsx(
     client.force_login(alice)
 
     response = client.get(
-        reverse("analyseia:resultat", args=[entreprise.siren, document.id]),
+        reverse("analyseia:resultat", args=[entreprise.siren, document.id, rendu]),
     )
 
     assert response["Content-Disposition"] == "filename=resultats.xlsx"
@@ -46,51 +48,66 @@ def test_telechargement_des_resultats_IA_d_un_document_au_format_xlsx(
     workbook = load_workbook(filename=BytesIO(response.content))
     onglet = workbook[">>>"]
     assert not onglet["C14"].value
-    onglet = workbook["Phrases relatives aux ESG"]
-    assert onglet["A1"].value == "Thème"
+    if rendu == "esrs":
+        onglet = workbook["Phrases relatives aux ESRS"]
+        assert onglet["A1"].value == "ESRS"
+    else:
+        onglet = workbook["Phrases relatives aux ESG"]
+        assert onglet["A1"].value == "Thème"
     assert onglet["B1"].value == "Fichier"
     assert onglet["C1"].value == "Page"
     assert onglet["D1"].value == "Phrase"
-    assert onglet["A2"].value == "Changement climatique"
+    if rendu == "esrs":
+        assert onglet["A2"].value == "ESRS E1 - Changement climatique"
+    else:
+        assert onglet["A2"].value == "Changement climatique"
     assert onglet["C2"].value == 1
     assert onglet["D2"].value == "A"
-    assert onglet["A3"].value == "Pollution"
+    if rendu == "esrs":
+        assert onglet["A3"].value == "ESRS E2 - Pollution"
+    else:
+        assert onglet["A3"].value == "Pollution"
     assert onglet["C3"].value == 6
     assert onglet["D3"].value == "B"
-    assert onglet["A4"].value == "Pollution"
+    if rendu == "esrs":
+        assert onglet["A3"].value == "ESRS E2 - Pollution"
+    else:
+        assert onglet["A4"].value == "Pollution"
     assert onglet["C4"].value == 7
     assert onglet["D4"].value == "C"
 
 
+@pytest.mark.parametrize("rendu", ["theme", "esrs"])
 def test_telechargement_des_resultats_IA_d_un_document_inexistant(
-    client, entreprise_factory, alice
+    rendu, client, entreprise_factory, alice
 ):
     entreprise = entreprise_factory(utilisateur=alice)
     client.force_login(alice)
 
     response = client.get(
-        reverse("analyseia:resultat", args=[entreprise.siren, 42]),
+        reverse("analyseia:resultat", args=[entreprise.siren, 42, rendu]),
     )
 
     assert response.status_code == 404
 
 
+@pytest.mark.parametrize("rendu", ["theme", "esrs"])
 def test_telechargement_des_resultats_IA_d_un_document_redirige_vers_la_connexion_si_non_connecté(
-    client, entreprise_factory, alice, analyse
+    rendu, client, entreprise_factory, alice, analyse
 ):
     entreprise = entreprise_factory(siren="000000089", utilisateur=alice)
 
     response = client.get(
-        reverse("analyseia:resultat", args=[entreprise.siren, analyse.id]),
+        reverse("analyseia:resultat", args=[entreprise.siren, analyse.id, rendu]),
     )
 
     assert response.status_code == 302
 
 
-def test_telechargement_des_resultats_ia_de_l_ensemble_des_documents_au_format_xlsx(
-    client, entreprise_factory, alice
+def test_telechargement_des_resultats_ia_de_l_ensemble_des_documents_au_format_xlsx_lié_à_une_entreprise(
+    client, entreprise_factory, csrd, alice
 ):
-    entreprise = entreprise_factory(utilisateur=alice)
+    entreprise = entreprise_factory(utilisateur=alice, siren="123456789")
     document = entreprise.analyses_ia.create(
         etat="success",
         resultat_json="""{
@@ -147,6 +164,81 @@ def test_telechargement_des_resultats_ia_de_l_ensemble_des_documents_au_format_x
     assert onglet["D3"].value == "B"
 
 
+def test_telechargement_des_resultats_ia_de_l_ensemble_des_documents_au_format_xlsx_lié_à_une_CSRD(
+    client, entreprise_factory, csrd, alice
+):
+    entreprise = entreprise_factory(utilisateur=alice, siren="123456789")
+    document = AnalyseIA.objects.create(
+        etat="success",
+        resultat_json="""{
+  "ESRS E1": [
+    {
+      "PAGES": 1,
+      "TEXTS": "A"
+    }
+   ],
+    "Non ESRS": [
+    {
+      "PAGES": 22,
+      "TEXTS": "X"
+    }
+  ]
+  }""",
+    )
+    document.rapports_csrd.add(csrd)
+
+    document = AnalyseIA.objects.create(
+        etat="success",
+        resultat_json="""{
+  "ESRS E2": [
+    {
+      "PAGES": 6,
+      "TEXTS": "B"
+    }
+  ]
+  }""",
+    )
+    document.rapports_csrd.add(csrd)
+    client.force_login(alice)
+
+    response = client.get(
+        reverse("analyseia:synthese_resultat", args=[entreprise.siren, csrd.id]),
+    )
+
+    assert response["Content-Disposition"] == "filename=synthese_resultats.xlsx"
+    assert (
+        response["content-type"]
+        == "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet"
+    )
+    workbook = load_workbook(filename=BytesIO(response.content))
+    onglet = workbook[">>>"]
+    assert onglet["C14"].value == "Synthèse générique"
+    onglet = workbook["Phrases relatives aux ESRS"]
+    assert onglet["A1"].value == "ESRS"
+    assert onglet["B1"].value == "Fichier"
+    assert onglet["C1"].value == "Page"
+    assert onglet["D1"].value == "Phrase"
+    assert onglet["A2"].value == "ESRS E1 - Changement climatique"
+    assert onglet["C2"].value == 1
+    assert onglet["D2"].value == "A"
+    assert onglet["A3"].value == "ESRS E2 - Pollution"
+    assert onglet["C3"].value == 6
+    assert onglet["D3"].value == "B"
+
+
+def test_telechargement_des_resultats_IA_de_l_ensemble_des_documents_d_un_rapport_csrd_inexistant(
+    client, entreprise_factory, alice
+):
+    entreprise = entreprise_factory(utilisateur=alice)
+    client.force_login(alice)
+
+    response = client.get(
+        reverse("analyseia:synthese_resultat", args=[entreprise.siren, 42]),
+    )
+
+    assert response.status_code == 404
+
+
 def test_telechargement_des_resultats_IA_de_l_ensemble_des_documents_redirige_vers_la_connexion_si_non_connecté(
     client, entreprise_factory, alice
 ):
@@ -158,8 +250,14 @@ def test_telechargement_des_resultats_IA_de_l_ensemble_des_documents_redirige_ve
 
     assert response.status_code == 302
 
+    response = client.get(
+        reverse("analyseia:synthese_resultat", args=[entreprise.siren, 42]),
+    )
 
-def test_telechargement_des_resultats_par_ESRS_au_format_xlsx(
+    assert response.status_code == 302
+
+
+def test_telechargement_des_resultats_par_ESRS_au_format_xlsx_lié_à_une_entreprise(
     client, entreprise_factory, alice
 ):
     entreprise = entreprise_factory(utilisateur=alice)
@@ -228,6 +326,77 @@ def test_telechargement_des_resultats_par_ESRS_au_format_xlsx(
     assert onglet["D3"].value == "C"
 
 
+def test_telechargement_des_resultats_par_ESRS_au_format_xlsx_lié_à_une_CSRD(
+    client, entreprise_factory, csrd, alice
+):
+    entreprise = entreprise_factory(utilisateur=alice, siren="123456789")
+    document = AnalyseIA.objects.create(
+        etat="success",
+        resultat_json="""{
+  "ESRS E1": [
+    {
+      "PAGES": 1,
+      "TEXTS": "A"
+    }
+  ],
+  "ESRS E2": [
+    {
+      "PAGES": 4,
+      "TEXTS": "B"
+    }
+  ],
+  "Non ESRS": [
+    {
+      "PAGES": 22,
+      "TEXTS": "X"
+    }
+  ]
+  }""",
+    )
+    document.rapports_csrd.add(csrd)
+
+    document = AnalyseIA.objects.create(
+        etat="success",
+        resultat_json="""{
+  "ESRS E2": [
+    {
+      "PAGES": 6,
+      "TEXTS": "C"
+    }
+  ]
+  }""",
+    )
+    document.rapports_csrd.add(csrd)
+    client.force_login(alice)
+
+    response = client.get(
+        reverse(
+            "analyseia:synthese_resultat_par_ESRS",
+            args=[entreprise.siren, "E2", csrd.id],
+        ),
+    )
+
+    assert response["Content-Disposition"] == "filename=resultats_ESRS_E2.xlsx"
+    assert (
+        response["content-type"]
+        == "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet"
+    )
+    workbook = load_workbook(filename=BytesIO(response.content))
+    onglet = workbook[">>>"]
+    assert onglet["C14"].value == "ESRS E2 - Pollution"
+    onglet = workbook["Phrases relatives aux ESRS"]
+    assert onglet["A1"].value == "ESRS"
+    assert onglet["B1"].value == "Fichier"
+    assert onglet["C1"].value == "Page"
+    assert onglet["D1"].value == "Phrase"
+    assert onglet["A2"].value == "ESRS E2 - Pollution"
+    assert onglet["C2"].value == 4
+    assert onglet["D2"].value == "B"
+    assert onglet["A3"].value == "ESRS E2 - Pollution"
+    assert onglet["C3"].value == 6
+    assert onglet["D3"].value == "C"
+
+
 def test_telechargement_des_resultats_IA_par_ESRS_d_une_entreprise_inexistante(
     client, alice
 ):
@@ -239,16 +408,32 @@ def test_telechargement_des_resultats_IA_par_ESRS_d_une_entreprise_inexistante(
 
     assert response.status_code == 404
 
+    response = client.get(
+        reverse("analyseia:synthese_resultat_par_ESRS", args=["123456789", "E2", 42]),
+    )
+
+    assert response.status_code == 404
+
 
 def test_telechargement_des_resultats_IA_par_ESRS_redirige_vers_la_connexion_si_non_connecté(
     client,
     entreprise_factory,
     alice,
+    csrd,
 ):
-    entreprise = entreprise_factory(utilisateur=alice)
+    entreprise = entreprise_factory(utilisateur=alice, siren="123456789")
 
     response = client.get(
         reverse("analyseia:synthese_resultat_par_ESRS", args=[entreprise.siren, "E2"]),
+    )
+
+    assert response.status_code == 302
+
+    response = client.get(
+        reverse(
+            "analyseia:synthese_resultat_par_ESRS",
+            args=[entreprise.siren, "E2", csrd.id],
+        ),
     )
 
     assert response.status_code == 302
