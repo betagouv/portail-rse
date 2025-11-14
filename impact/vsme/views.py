e88a0f617a43cd9592bdba1e8dd0734aeb559aff
Stéphane <stephane@yaal.coop>
2025-11-14 11:37:18 +0100

    wip: refacto: arrivée de export_indicateurs

--- impact/vsme/views.py
+++ impact/vsme/views.py
@@ -394,7 +394,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_choix_unique(indicateur, worksheet, "A4")
+            _export_indicateur(indicateur, worksheet, "A4")
         elif indicateur_schema_id == "B1-24-b":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -402,7 +402,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_choix_multiple(indicateur, worksheet, "B4")
+            _export_indicateur(indicateur, worksheet, "B4")
         elif indicateur_schema_id == "B1-24-c":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -410,7 +410,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_choix_unique(indicateur, worksheet, "C4")
+            _export_indicateur(indicateur, worksheet, "C4")
         elif indicateur_schema_id == "B1-24-d":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -418,7 +418,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_tableau(indicateur, worksheet, "D4")
+            _export_indicateur(indicateur, worksheet, "D4")
         elif indicateur_schema_id == "B1-24-e-i":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -434,7 +434,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_choix_multiple(indicateur, worksheet, "J4")
+            _export_indicateur(indicateur, worksheet, "J4")
         elif indicateur_schema_id == "B1-24-e-iii":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -442,7 +442,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_choix_unique(indicateur, worksheet, "K4")
+            _export_indicateur(indicateur, worksheet, "K4")
         elif indicateur_schema_id == "B1-24-e-v":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -458,7 +458,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_choix_multiple(indicateur, worksheet, "M4")
+            _export_indicateur(indicateur, worksheet, "M4")
         elif indicateur_schema_id == "B1-24-e-vii":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -466,7 +466,7 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_tableau(indicateur, worksheet, "N4")
+            _export_indicateur(indicateur, worksheet, "N4")
         elif indicateur_schema_id == "B1-25":
             try:
                 indicateur = rapport_vsme.indicateurs.get(
@@ -474,7 +474,18 @@ def _export_b1(workbook, rapport_vsme):
                 )
             except ObjectDoesNotExist:
                 break
-            _export_tableau(indicateur, worksheet, "T4")
+            _export_indicateur(indicateur, worksheet, "T4")
+
+
+def _export_indicateur(indicateur, worksheet, cellule_depart):
+    type_indicateur = indicateur.schema["champs"][0]["type"]
+    match type_indicateur:
+        case "choix_unique" | "nombre_entier":
+            _export_choix_unique(indicateur, worksheet, cellule_depart)
+        case "choix_multiple":
+            _export_choix_multiple(indicateur, worksheet, cellule_depart)
+        case "tableau":
+            _export_tableau(indicateur, worksheet, cellule_depart)
 
 
 def _export_choix_unique(indicateur, worksheet, cellule):
