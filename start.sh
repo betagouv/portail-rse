4c49fec78c74e66d45f9accb37987b77bd8ded12
Frédéric Vergez <fred@ikarius.com>
2025-12-09 14:47:10 +0100

    feat: ajout du buildpack nginx et filtrage

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
+set -e
 
+echo "-----> Running collectstatic"
 python impact/manage.py collectstatic --no-input
 
-# En supposant qu'il y avait au moins 8 cores sur la machine et d'après la doc de gunicorn le nombre de workers conseillé était de (2 x $num_cores) + 1
-# https://docs.gunicorn.org/en/stable/design.html#how-many-workers
-# Ce n'était pas concluant (explosion de la mémoire avec l'option --workers=17)
-gunicorn --pythonpath=impact impact.wsgi --log-file -
+echo "-----> Starting gunicorn on unix socket"
+# Démarrer gunicorn sur un socket Unix (nginx fait proxy depuis $PORT)
+gunicorn impact.wsgi --pythonpath=impact --bind unix:/tmp/gunicorn.sock --log-file -
