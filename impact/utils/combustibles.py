bb426e06ebc915569487661f403b58e95379d184
emillumine <camille@yaal.coop>
2025-12-12 16:56:11 +0100

    wip: ajoute l'indicateur combustibles de B3

---
+++ impact/utils/combustibles.py
@@ -0,0 +1,252 @@
+COMBUSTIBLES = {
+    "Anthracite": {
+        "etat_chimique": "solide",
+        "NCV": 26.70,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Aviation gasoline": {
+        "etat_chimique": "liquide",
+        "NCV": 44.30,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Biodiesel": {
+        "etat_chimique": "liquide",
+        "NCV": 27.00,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Biogasoline": {
+        "etat_chimique": "liquide",
+        "NCV": 27.00,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Bitumen": {
+        "etat_chimique": "liquide",
+        "NCV": 40.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Blast furnace gas": {
+        "etat_chimique": "gaz",
+        "NCV": 2.47,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Brown coal briquettes": {
+        "etat_chimique": "solide",
+        "NCV": 20.70,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Butane and isomers": {
+        "etat_chimique": "gaz",
+        "NCV": 45.57,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Coke oven coke & lignite coke": {
+        "etat_chimique": "gaz",
+        "NCV": 28.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Coke oven gas": {
+        "etat_chimique": "gaz",
+        "NCV": 38.70,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Coking coal": {
+        "etat_chimique": "solide",
+        "NCV": 28.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Crude oil and distillates": {
+        "etat_chimique": "liquide",
+        "NCV": 42.30,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Ethane": {
+        "etat_chimique": "gaz",
+        "NCV": 46.40,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Gas coke": {
+        "etat_chimique": "gaz",
+        "NCV": 28.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Gas/Diesel oil": {
+        "etat_chimique": "liquide",
+        "NCV": 43.00,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Jet gasoline": {
+        "etat_chimique": "liquide",
+        "NCV": 44.30,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Jet kerosene": {
+        "etat_chimique": "liquide",
+        "NCV": 44.10,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Kerosene": {
+        "etat_chimique": "liquide",
+        "NCV": 43.80,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Landfill gas": {
+        "etat_chimique": "gaz",
+        "NCV": 50.40,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Lignite": {
+        "etat_chimique": "solide",
+        "NCV": 11.90,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Liquefied Petroleum Gases (LPG)": {
+        "etat_chimique": "liquide",
+        "NCV": 47.30,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Lubricants": {
+        "etat_chimique": "liquide",
+        "NCV": 40.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Methane": {
+        "etat_chimique": "gaz",
+        "NCV": 50.00,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Motor gasoline/Petrol": {
+        "etat_chimique": "liquide",
+        "NCV": 44.30,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Municipal wastes (biomass fraction)": {
+        "etat_chimique": "solide",
+        "NCV": 11.60,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Municipal wastes (non-biomass fraction)": {
+        "etat_chimique": "solide",
+        "NCV": 10.00,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Naphtha": {
+        "etat_chimique": "liquide",
+        "NCV": 44.50,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Natural gas": {
+        "etat_chimique": "gaz",
+        "NCV": 48.00,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Natural gas liquids (NGL)": {
+        "etat_chimique": "liquide",
+        "NCV": 44.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Oil shale and tar sands": {
+        "etat_chimique": "liquide",
+        "NCV": 8.90,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Orimulsion": {
+        "etat_chimique": "liquide",
+        "NCV": 27.50,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Oxygen steel furnace gas": {
+        "etat_chimique": "gaz",
+        "NCV": 7.06,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Paraffin waxes": {
+        "etat_chimique": "solide",
+        "NCV": 40.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Patent fuel": {
+        "etat_chimique": "solide",
+        "NCV": 20.70,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Peat": {
+        "etat_chimique": "solide",
+        "NCV": 9.76,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Petroleum coke": {
+        "etat_chimique": "solide",
+        "NCV": 32.50,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Propane": {
+        "etat_chimique": "gaz",
+        "NCV": 46.32,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Refinery feedstocks": {
+        "etat_chimique": "liquide",
+        "NCV": 43.00,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Refinery Gas": {
+        "etat_chimique": "gaz",
+        "NCV": 49.50,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Residual fuel oil": {
+        "etat_chimique": "liquide",
+        "NCV": 40.40,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Shale oil (liquid)": {
+        "etat_chimique": "liquide",
+        "NCV": 38.10,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Sludge gas": {
+        "etat_chimique": "gaz",
+        "NCV": 50.40,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Sub-bituminous coal": {
+        "etat_chimique": "solide",
+        "NCV": 18.90,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Sulphite lyes (black liquor)": {
+        "etat_chimique": "liquide",
+        "NCV": 11.80,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Turpentine": {
+        "etat_chimique": "liquide",
+        "NCV": 44.40,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "Vegetable oils": {
+        "etat_chimique": "liquide",
+        "NCV": 37.80,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Waste oils": {
+        "etat_chimique": "liquide",
+        "NCV": 40.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Waste water treatment biogas": {
+        "etat_chimique": "gaz",
+        "NCV": 35.00,
+        "etat_renouvelabilite": "renouvelable",
+    },
+    "White Spirit & SBP": {
+        "etat_chimique": "liquide",
+        "NCV": 40.20,
+        "etat_renouvelabilite": "non_renouvelable",
+    },
+    "Wood/Wood waste": {
+        "etat_chimique": "solide",
+        "NCV": 15.60,
+        "etat_renouvelabilite": "renouvelable",
+    },
+}
