085f1dcc2b66732432e03ac93bc8df49eca5f449
emillumine <camille@yaal.coop>
2026-03-03 17:56:31 +0100

    restreint l'accès à la gestion des réglementations du tableau de bord aux entreprises qualifiées

--- impact/reglementations/views/__init__.py
+++ impact/reglementations/views/__init__.py
@@ -9,6 +9,7 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
+from entreprises.decorators import entreprise_qualifiee_requise
 from entreprises.decorators import entreprise_requise
 from entreprises.models import CaracteristiquesAnnuelles
 from habilitations.views import contributeurs_context
@@ -195,35 +196,10 @@ def analyse_double_materialite(request, entreprise):
 
 
 @login_required
-@entreprise_requise
+@entreprise_qualifiee_requise
 @log_path("app:reglementations")
 def reglementations(request, entreprise):
-    caracteristiques = (
-        entreprise.dernieres_caracteristiques_qualifiantes
-        or entreprise.dernieres_caracteristiques
-    )
-
-    # Gérer le cas où il n'y a aucune caractéristique
-    if not caracteristiques:
-        messages.warning(
-            request,
-            "Veuillez renseigner le profil de l'entreprise pour accéder aux réglementations.",
-        )
-        return redirect("entreprises:qualification", siren=entreprise.siren)
-
-    # Afficher un message d'info si le profil est incomplet
-    if not caracteristiques.sont_qualifiantes:
-        messages.info(
-            request,
-            f"Le profil de votre entreprise est incomplet. Certaines réglementations ne pourront pas être calculées. "
-            f"<a href='{reverse_lazy('entreprises:qualification', args=[entreprise.siren])}'>Compléter le profil</a>",
-        )
-    # Afficher un avertissement si les caractéristiques ne sont pas à jour
-    elif caracteristiques != entreprise.caracteristiques_actuelles():
-        messages.warning(
-            request,
-            f"Les informations affichées sont basées sur l'exercice comptable {caracteristiques.annee}. <a href='{reverse_lazy('entreprises:qualification', args=[entreprise.siren])}'>Mettre à jour le profil de l'entreprise.</a>",
-        )
+    caracteristiques = entreprise.dernieres_caracteristiques_qualifiantes
 
     reglementations = calcule_reglementations(caracteristiques)
     reglementations_a_actualiser = [
@@ -256,11 +232,6 @@ def reglementations(request, entreprise):
         for r in reglementations
         if r["status"].status == ReglementationStatus.STATUS_RECOMMANDE
     ]
-    reglementations_incalculables = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_INCALCULABLE
-    ]
 
     context = tableau_de_bord_menu_context(entreprise)
     context |= {
@@ -269,8 +240,7 @@ def reglementations(request, entreprise):
         "reglementations_a_jour": reglementations_a_jour,
         "autres_reglementations": reglementations_soumises
         + reglementations_recommandees
-        + reglementations_non_soumises
-        + reglementations_incalculables,
+        + reglementations_non_soumises,
     }
     return render(
         request,
@@ -291,7 +261,7 @@ def calcule_reglementations(caracteristiques: CaracteristiquesAnnuelles):
 
 
 @login_required
-@entreprise_requise
+@entreprise_qualifiee_requise
 def reglementation(request, entreprise, id_reglementation):
     reglementation = None
     for r in REGLEMENTATIONS:
@@ -301,19 +271,7 @@ def reglementation(request, entreprise, id_reglementation):
     if not reglementation:
         raise Http404
 
-    caracteristiques = (
-        entreprise.dernieres_caracteristiques_qualifiantes
-        or entreprise.dernieres_caracteristiques
-    )
-
-    # Gérer le cas où il n'y a aucune caractéristique
-    if not caracteristiques:
-        messages.warning(
-            request,
-            "Veuillez renseigner le profil de l'entreprise pour voir cette réglementation.",
-        )
-        return redirect("entreprises:qualification", siren=entreprise.siren)
-
+    caracteristiques = entreprise.dernieres_caracteristiques_qualifiantes
     status = reglementation.calculate_status(caracteristiques)
 
     template_name = f"reglementations/tableau_de_bord/{id_reglementation}.html"
