f48b23fe2e11a4b3b4d8c71bfb34b34df0bea392
Stéphane <stephane@yaal.coop>
2026-03-10 10:45:06 +0100

    test: suppresssion de fixtures inutiles

--- impact/oidc/tests/test_views.py
+++ impact/oidc/tests/test_views.py
@@ -1,4 +1,3 @@
-import pytest
 from django.urls import reverse
 
 from entreprises.models import Entreprise
@@ -6,7 +5,6 @@ from habilitations.enums import UserRole
 from habilitations.models import Habilitation
 
 
-@pytest.mark.django_db()
 def test_proconnect_dispatch_view_cree_l_entreprise_sélectionnée_et_ajoute_l_habilitation(
     client, alice_sur_proconnect
 ):
@@ -30,7 +28,6 @@ def test_proconnect_dispatch_view_cree_l_entreprise_sélectionnée_et_ajoute_l_h
     assert (reverse("users:post_login_dispatch"), 302) in response.redirect_chain
 
 
-@pytest.mark.django_db()
 def test_proconnect_dispatch_view_ajoute_l_habilitation_à_l_entreprise_déjà_existante(
     client, entreprise_factory, alice_sur_proconnect
 ):
@@ -52,7 +49,6 @@ def test_proconnect_dispatch_view_ajoute_l_habilitation_à_l_entreprise_déjà_e
     assert response.redirect_chain[0] == ("/post-login-dispatch", 302)
 
 
-@pytest.mark.django_db()
 def test_proconnect_dispatch_view_ne_fait_que_rediriger_si_habilitation_et_entreprise_déjà_existantes(
     client, entreprise_factory, alice_sur_proconnect
 ):
