cfe5f7d480d1de326aebbbf216b9f53ac2462954
Stéphane <stephane@yaal.coop>
2026-04-02 15:10:47 +0200

    script d'ajout du code postal des entreprises existantes

---
+++ impact/entreprises/management/commands/force_code_postal.py
@@ -0,0 +1,36 @@
+import time
+
+from django.core.management.base import BaseCommand
+
+import api.exceptions
+import api.recherche_entreprises
+from entreprises.models import Entreprise
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for entreprise in Entreprise.objects.all():
+            if entreprise.code_postal:
+                print(
+                    f"IGNORE: {entreprise.siren} {entreprise.denomination} {entreprise.code_postal}"
+                )
+            else:
+                try:
+                    maj(entreprise)
+                    print(
+                        f"OK: {entreprise.siren} {entreprise.denomination} {entreprise.code_postal}"
+                    )
+                except api.exceptions.TooManyRequestError as e:
+                    time.sleep(1)
+                    maj(entreprise)
+                    print(
+                        f"OK (2): {entreprise.siren} {entreprise.denomination} {entreprise.code_postal}"
+                    )
+                except api.exceptions.APIError as e:
+                    print(f"ERREUR {e}: {entreprise.siren}")
+
+
+def maj(entreprise):
+    infos_entreprise = api.recherche_entreprises.recherche_par_siren(entreprise.siren)
+    entreprise.code_postal = infos_entreprise["code_postal"]
+    entreprise.save()
