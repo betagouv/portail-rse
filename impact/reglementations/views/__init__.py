2ea47cbfabfa1ed8905441fda00a1f35720cb907
Stéphane <stephane@yaal.coop>
2026-08-11 18:13:57 +0200

    refacto: déplacement de fonctions vers un module commun
    Préparation de l'arrivée de la fonctionnalité permettant aux utilisateurs d'expérimenter des situations différentes de l'entreprise et ses impacts en termes de réglementations applicables.

--- impact/reglementations/views/__init__.py
+++ impact/reglementations/views/__init__.py
@@ -6,12 +6,11 @@ from django.http import Http404
 from django.shortcuts import render
 
 from .tableau_de_bord import tableau_de_bord_menu_context
+from .toutes_reglementations import etats_reglementations_selon_caracteristiques
 from .toutes_reglementations import REGLEMENTATIONS
 from entreprises.decorators import entreprise_qualifiee_requise
-from entreprises.models import CaracteristiquesAnnuelles
 from logs import event_logger as logger
 from logs import log_path
-from reglementations.views.base import ReglementationStatus
 from reglementations.views.csrd.csrd import rapport_csrd
 from vsme.models import RapportVSME
 
@@ -58,64 +57,14 @@ def calculer_metriques_entreprise(entreprise):
 @log_path("app:reglementations")
 def reglementations(request, entreprise):
     caracteristiques = entreprise.dernieres_caracteristiques_qualifiantes
-
-    reglementations = calcule_reglementations(caracteristiques)
-    reglementations_a_actualiser = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_A_ACTUALISER
-    ]
-    reglementations_en_cours = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_EN_COURS
-    ]
-    reglementations_a_jour = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_A_JOUR
-    ]
-    reglementations_soumises = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_SOUMIS
-    ]
-    reglementations_non_soumises = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_NON_SOUMIS
-    ]
-    reglementations_recommandees = [
-        r
-        for r in reglementations
-        if r["status"].status == ReglementationStatus.STATUS_RECOMMANDE
-    ]
-
+    reglementations = etats_reglementations_selon_caracteristiques(caracteristiques)
     context = tableau_de_bord_menu_context(entreprise)
-    context |= {
-        "reglementations_a_actualiser": reglementations_a_actualiser,
-        "reglementations_en_cours": reglementations_en_cours,
-        "reglementations_a_jour": reglementations_a_jour,
-        "autres_reglementations": reglementations_soumises
-        + reglementations_recommandees
-        + reglementations_non_soumises,
-    }
+    context.update(reglementations)
     return render(
         request,
         "reglementations/tableau_de_bord/reglementations.html",
         context=context,
     )
-
-
-def calcule_reglementations(caracteristiques: CaracteristiquesAnnuelles):
-    reglementations = [
-        {
-            "reglementation": reglementation,
-            "status": reglementation.calculate_status(caracteristiques),
-        }
-        for reglementation in REGLEMENTATIONS
-    ]
-    return reglementations
 
 
 @login_required
