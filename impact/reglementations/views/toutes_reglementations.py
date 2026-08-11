fe9b1b02cea2133d7e158b517f9fb39210c1f61e
Stéphane <stephane@yaal.coop>
2026-08-11 18:13:57 +0200

    refacto: déplacement de fonctions vers un module commun
    Préparation de l'arrivée de la fonctionnalité permettant aux utilisateurs d'expérimenter des situations différentes de l'entreprise et ses impacts en termes de réglementations applicables.

--- impact/reglementations/views/toutes_reglementations.py
+++ impact/reglementations/views/toutes_reglementations.py
@@ -1,5 +1,7 @@
+from entreprises.models import CaracteristiquesAnnuelles
 from reglementations.utils import VSMEReglementation
 from reglementations.views.audit_energetique import AuditEnergetiqueReglementation
+from reglementations.views.base import ReglementationStatus
 from reglementations.views.bdese import BDESEReglementation
 from reglementations.views.bges import BGESReglementation
 from reglementations.views.csrd import CSRDReglementation
@@ -19,3 +21,57 @@ REGLEMENTATIONS = [
     DispositifAntiCorruption,
     PlanVigilanceReglementation,
 ]
+
+
+def calcule_reglementations(caracteristiques: CaracteristiquesAnnuelles):
+    reglementations = [
+        {
+            "reglementation": reglementation,
+            "status": reglementation.calculate_status(caracteristiques),
+        }
+        for reglementation in REGLEMENTATIONS
+    ]
+    return reglementations
+
+
+def etats_reglementations_selon_caracteristiques(caracteristiques):
+    reglementations = calcule_reglementations(caracteristiques)
+    reglementations_a_actualiser = [
+        r
+        for r in reglementations
+        if r["status"].status == ReglementationStatus.STATUS_A_ACTUALISER
+    ]
+    reglementations_en_cours = [
+        r
+        for r in reglementations
+        if r["status"].status == ReglementationStatus.STATUS_EN_COURS
+    ]
+    reglementations_a_jour = [
+        r
+        for r in reglementations
+        if r["status"].status == ReglementationStatus.STATUS_A_JOUR
+    ]
+    reglementations_soumises = [
+        r
+        for r in reglementations
+        if r["status"].status == ReglementationStatus.STATUS_SOUMIS
+    ]
+    reglementations_non_soumises = [
+        r
+        for r in reglementations
+        if r["status"].status == ReglementationStatus.STATUS_NON_SOUMIS
+    ]
+    reglementations_recommandees = [
+        r
+        for r in reglementations
+        if r["status"].status == ReglementationStatus.STATUS_RECOMMANDE
+    ]
+
+    return {
+        "reglementations_a_actualiser": reglementations_a_actualiser,
+        "reglementations_en_cours": reglementations_en_cours,
+        "reglementations_a_jour": reglementations_a_jour,
+        "autres_reglementations": reglementations_soumises
+        + reglementations_recommandees
+        + reglementations_non_soumises,
+    }
