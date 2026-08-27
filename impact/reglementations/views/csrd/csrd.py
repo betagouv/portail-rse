24f08cc7908f515192f9aa1243f63f927e939a31
Stéphane <stephane@yaal.coop>
2026-08-27 16:02:40 +0200

    ajout troisième onglet pour visualiser les analyses IA v2

--- impact/reglementations/views/csrd/csrd.py
+++ impact/reglementations/views/csrd/csrd.py
@@ -637,7 +637,7 @@ def contexte_d_etape(id_etape, csrd, form=None):
                 "synthese_v1": synthese_analyse_v1(
                     csrd.analyses_ia.reussies(), prefixe_ESRS=True
                 ),
-                "onglet_resultats_actif": csrd.analyses_ia.reussies().exists()
+                "onglet_resultats_enjeux_actif": csrd.analyses_ia.reussies().exists()
                 and not csrd.analyses_ia.non_lancees(),
             }
         case "redaction-rapport-durabilite":
